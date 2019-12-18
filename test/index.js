638d13c46a2e5e22b1e6ff1d759b951505b3390e
Jim de Beer <beerdejim@gmail.com>
2019-12-18 18:11:42 +0100

    start of ts

---
+++ test/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ava_1 = require("ava");
+ava_1.default('one plus two equals three', t => {
+    t.is(1 + 2, 3);
+});
+//# sourceMappingURL=index.js.map
