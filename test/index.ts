9f7d7d5a1910ec164e990166e99afadb423d8d4f
Jim de Beer <beerdejim@gmail.com>
2020-01-07 13:17:10 +0100

    comments

--- test/index.ts
+++ test/index.ts
@@ -11,5 +11,6 @@ test('generates a unique id', async t => {
   const id2 = client.id({ type: 'match' })
   t.true(id1 !== id2)
   t.true(/ma.+/.test(id1))
-  // new types what this means is that the client allways needs to load a map
+  // new types what this means is that the client allways needs to load a map add it to prefix
+  // allways subscribe on it (hash)
 })
