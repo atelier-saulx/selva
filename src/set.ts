250ae058078beaeebc95baae7a6ab2ce2cc59d6a
Jim de Beer <beerdejim@gmail.com>
2020-01-07 17:15:55 +0100

    set

--- src/set.ts
+++ src/set.ts
@@ -11,7 +11,15 @@ type FnSet = {
 // maybe make item
 
 function set(payload: FnSet): void {
-  console.log('no', this)
+  console.log('yesh', this.redis)
+  if (!payload.$id) {
+    console.log('create item')
+    if (!payload.type) {
+      throw new Error('Cannot create an item if type is not provided')
+    }
+    // externalID
+    // make root on start up?
+  }
 }
 
 export default set
