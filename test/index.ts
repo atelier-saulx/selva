be189a54bd476df9bea8a3908c8123cffe288100
Jim de Beer <beerdejim@gmail.com>
2020-01-07 15:52:05 +0100

    index

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,29 @@
 import test from 'ava'
 // expose types here (default), maybe expose things like id etc
 import { connect } from '../src/index'
+import { start } from 'selva-server'
+
+let db
+
+test.before(async t => {
+  db = await start({ port: 6061, modules: ['redisearch'] })
+  // This runs before all tests
+})
 
 test('generates a unique id', async t => {
+  const client = connect({
+    port: 6060,
+    host: '127.0.1.1'
+  })
+  const id1 = client.id({ type: 'match' })
+  const id2 = client.id({ type: 'match' })
+  t.true(id1 !== id2)
+  t.true(/ma.+/.test(id1))
+  // new types what this means is that the client allways needs to load a map add it to prefix
+  // allways subscribe on it (hash)
+})
+
+test('set', async t => {
   const client = connect({
     port: 6060,
     host: '127.0.1.1'
