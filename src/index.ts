a9d7d5fe8081cf72a5fa3b48c696c5e58ec2edec
Tony Kovanen <tony.kovanen@gmail.com>
2020-01-07 08:12:05 +0200

    very small start to connect

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-type TypeA = { a: boolean, b: number }
-type TypeB = { a: boolean, c: string}
+import { default as RedisClient, ConnectOptions } from './redis'
+
+export class SelvaClient {
+  public redis: RedisClient
+
+  constructor(opts: ConnectOptions | (() => Promise<ConnectOptions>)) {
+    this.redis = new RedisClient(opts)
+  }
+}
+
+export function connect(
+  opts: ConnectOptions | (() => Promise<ConnectOptions>)
+): SelvaClient {
+  return new SelvaClient(opts)
+}
+
+// TODO: remove these, they are just for testing that the AVA typescript setup works. replace with real tests
+type TypeA = { a: boolean; b: number }
+type TypeB = { a: boolean; c: string }
 
 export function funA(obj: TypeA): TypeA {
-    return Object.assign(obj, { b: obj.b + 1 })
+  return Object.assign(obj, { b: obj.b + 1 })
 }
 
 export function funB(obj: TypeB): TypeB {
-    return Object.assign(obj, { a: !obj.a, c: obj.c + obj.c }) 
+  return Object.assign(obj, { a: !obj.a, c: obj.c + obj.c })
 }
 
 console.log(`Hello world`)
---
+++ src/redis/index.ts
@@ -0,0 +1,63 @@
+import { promisify } from 'util'
+import { createClient, RedisClient as Redis, Commands } from 'redis'
+
+export type ConnectOptions = {
+  port: number
+  host: string
+  retryStrategy: () => number
+}
+
+type RedisCommand = {
+  command: string
+  args: string[]
+  resolve: (x: any) => void
+  reject: (x: Error) => void
+}
+
+export default class RedisClient {
+  private connector: () => Promise<ConnectOptions>
+  private client: Redis
+  private buffer: RedisCommand[]
+
+  constructor(connect: ConnectOptions | (() => Promise<ConnectOptions>)) {
+    this.connector =
+      typeof connect === 'object' ? () => Promise.resolve(connect) : connect
+  }
+
+  private async connect() {
+    const opts = await this.connector()
+    this.client = createClient(opts)
+    await this.flushBuffered()
+  }
+
+  private async queue(
+    command: string,
+    args: string[],
+    resolve: (x: any) => void,
+    reject: (x: Error) => void
+  ) {
+    this.buffer.push({
+      command,
+      args,
+      resolve,
+      reject
+    })
+  }
+
+  // FIXME: this is not ready
+  private async flushBuffered() {
+    for (const cmd of this.buffer) {
+      await new Promise((resolve, _) => {
+        this.client.sendCommand(cmd.command, cmd.args, (err, reply) => {
+          if (err) {
+            cmd.reject(err)
+          } else {
+            cmd.resolve(reply)
+          }
+
+          resolve()
+        })
+      })
+    }
+  }
+}
