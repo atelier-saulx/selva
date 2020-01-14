115447638f8fca4c520443c5faacb2c07721174b
Jim de Beer <beerdejim@gmail.com>
2020-01-14 16:31:35 +0100

    selva

--- src/get/inherit.ts
+++ src/get/inherit.ts
@@ -4,32 +4,53 @@ import { Inherit, GetResult, GetOptions, GetItem, getInner } from './'
 import { getNestedField, setNestedResult } from './nestedFields'
 import isEmpty from './isEmpty'
 
-const lookUp = async (client: SelvaClient, field: string) => {}
+type Ancestor = {
+  id: Id
+  parents: Ancestor[]
+  depth?: number
+}
 
-const inheritTrue = async (
-  client: SelvaClient,
-  id: Id,
-  field: string,
-  result: GetResult,
-  language?: Language,
-  passed: Record<Id, true> = {}
-) => {
-  const parents = await client.redis.smembers(id + '.parents')
-  console.log(parents)
-  const props = {
-    $inherit: true
+const countDepth = (ancestor: Ancestor): number => {
+  let d = 0
+  if (ancestor.depth !== void 0) {
+    return ancestor.depth
   }
-
-  // zip up
-  for (let pId of parents) {
-    await getInner(client, props, result, pId, field, language)
-    const value = getNestedField(result, field)
-    if (!isEmpty(value)) {
-      return
+  if (ancestor.parents.length === 0) {
+    d = 0
+  } else {
+    d = 1
+    let pd = 0
+    for (let a of ancestor.parents) {
+      let x = countDepth(a)
+      if (x > pd) {
+        pd = x
+      }
+      d += pd
     }
   }
+  ancestor.depth = d
+  return d
+}
 
-  // passed
+const createAncestors = async (
+  client: SelvaClient,
+  id: Id
+): Promise<Ancestor> => {
+  const parents = await client.redis.smembers(id + '.parents')
+  const ancestor: Ancestor = { id, parents: [], depth: 0 }
+  if (parents.length) {
+    ancestor.depth = 1
+    let pd = 0
+    for (let pId of parents) {
+      const a = await createAncestors(client, pId)
+      if (a.depth > pd) {
+        pd = a.depth
+      }
+      ancestor.parents.push(a)
+    }
+    ancestor.depth = pd
+  }
+  return ancestor
 }
 
 const inherit = async (
@@ -44,7 +65,8 @@ const inherit = async (
   console.log('snurkels', inherit, props)
   const value = getNestedField(result, field)
   if (inherit === true && isEmpty(value)) {
-    await inheritTrue(client, id, field, result, language)
+    const a = await createAncestors(client, id)
+    console.log('ANCESTORS', JSON.stringify(a, void 0, 2))
   }
 }
 
