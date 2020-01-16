28c4e8e087a7e09dbb30e93d3810d62e626ed4db
Tony Kovanen <tony.kovanen@gmail.com>
2020-01-16 18:23:40 +0700

    remove debug prints

--- lua/src/modify/ancestors.ts
+++ lua/src/modify/ancestors.ts
@@ -6,7 +6,6 @@ export function getNewAncestors(parents: Id[], from?: Id[]): string[] {
   let allAncestors: { [k: string]: boolean } = {}
   for (let i = 0; i < parents.length; i++) {
     let ancestors = redis.hget(parents[i], 'ancestors')
-    redis.debug('ANCESTORS ' + ancestors + ' OF ' + parents[i])
     if (ancestors && ancestors.length > 0) {
       const arr = splitString(ancestors, ',')
       if (arr.length > 0) {
@@ -44,17 +43,7 @@ export function reCalculateAncestors(id: Id, parents?: Id[]) {
     parents = redis.smembers(id + '.parents')
   }
 
-  redis.debug('ENTRY: ' + id)
-  redis.debug('PARENTS (' + tostring(parents.length) + '):')
-  for (let i = 0; i < parents.length; i++) {
-    redis.debug(parents[i])
-  }
-
   let ancestors = getNewAncestors(parents)
-  redis.debug('ANCESTORS (' + tostring(ancestors.length) + '):')
-  redis.debug(joinString(ancestors, ','))
-  redis.debug('')
-
   redis.hset(id, 'ancestors', joinString(ancestors, ','))
   const children = redis.smembers(id + '.children')
   for (let child of children) {
