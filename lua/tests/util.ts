e2b39d73255afe43c81a8e87ec971be1daafbeed
Tony Kovanen <tony.kovanen@gmail.com>
2020-01-17 19:44:05 +0700

    add some assert messages

--- lua/tests/util.ts
+++ lua/tests/util.ts
@@ -7,26 +7,26 @@ import {
 } from '../src/util'
 
 export function test_arrayIsEqual() {
-  testy_assert(arrayIsEqual([1, 2, 3], [1, 2, 3]))
-  testy_assert(!arrayIsEqual([1, 2, 3], [1, 2]))
-  testy_assert(!arrayIsEqual([1, 2, 3], [1, 3, 2]))
+  testy_assert(arrayIsEqual([1, 2, 3], [1, 2, 3]), '[1,2,3] === [1,2,3]')
+  testy_assert(!arrayIsEqual([1, 2, 3], [1, 2]), '[1,2,3] !== [1,2]')
+  testy_assert(!arrayIsEqual([1, 2, 3], [1, 3, 2]), '[1,2,3] !== [1,3,2]')
 }
 
 export function test_isArrayWithPrimitives() {
-  testy_assert(isArray(true) === false)
-  testy_assert(isArray(123) === false)
-  testy_assert(isArray('hello') === false)
+  testy_assert(isArray(true) === false, '[true] not array')
+  testy_assert(isArray(123) === false, '123 not array')
+  testy_assert(isArray('hello') === false, '"hello" not array')
 }
 
 export function test_isArrayWithObjects() {
-  testy_assert(isArray({ a: 1, b: 2, c: 3 }) === false)
+  testy_assert(isArray({ a: 1, b: 2, c: 3 }) === false, 'object not array')
   // testy_assert(isArray({}) === false) // this is an unfortunate case we can't really detect, but we can always assume it's an empty array in these cases
 }
 
 export function test_isArrayWithArray() {
-  testy_assert(isArray([1, 2]) === true)
-  testy_assert(isArray([1]) === true)
-  testy_assert(isArray([]) === true)
+  testy_assert(isArray([1, 2]) === true, '[1,2] is array')
+  testy_assert(isArray([1]) === true, '[1] is array')
+  testy_assert(isArray([]) === true, '[] is array')
 }
 
 export function test_ensureArrayWithArray() {
@@ -53,13 +53,25 @@ export function test_ensureArrayWithNonArrays() {
 }
 
 export function test_joinString() {
-  testy_assert(joinString(['lekker', 'man'], '|') === 'lekker|man')
-  testy_assert(joinString(['sick'], '|') === 'sick')
-  testy_assert(joinString([], '|') === '')
+  testy_assert(
+    joinString(['lekker', 'man'], '|') === 'lekker|man',
+    'can join array with multiple elements'
+  )
+  testy_assert(
+    joinString(['sick'], '|') === 'sick',
+    'can join array with single element'
+  )
+  testy_assert(joinString([], '|') === '', 'can join empty array')
 }
 
 export function test_splitString() {
-  testy_assert(arrayIsEqual(splitString('lekker man', ' '), ['lekker', 'man']))
-  testy_assert(arrayIsEqual(splitString('lekker', ' '), ['lekker']))
-  testy_assert(arrayIsEqual(splitString('', ' '), []))
+  testy_assert(
+    arrayIsEqual(splitString('lekker man', ' '), ['lekker', 'man']),
+    'can split string with 2 split results'
+  )
+  testy_assert(
+    arrayIsEqual(splitString('lekker', ' '), ['lekker']),
+    'can split string with nothing to split but has data'
+  )
+  testy_assert(arrayIsEqual(splitString('', ' '), []), 'can split empty string')
 }
