ad5f281f92d67b75d651c58df2c5f0c5e3818ba1
Jim de Beer <beerdejim@gmail.com>
2020-01-22 00:47:27 +0100

    url validator

--- src/set/fieldParsers/simple.ts
+++ src/set/fieldParsers/simple.ts
@@ -1,12 +1,26 @@
 import { SetOptions } from '../types'
 import { TypeSchema, FieldSchemaOther } from '../../schema'
 
+const isUrlRe = new RegExp(
+  '^(https?:\\/\\/)?' + // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$',
+  'i'
+)
+
+const validURL = (str: string): boolean => {
+  return isUrlRe.test(str)
+}
+
 const verifiers = {
   string: (payload: string) => {
     return typeof payload === 'string'
   },
   url: (payload: string) => {
-    return typeof payload === 'string'
+    return typeof payload === 'string' && validURL(payload)
   },
   number: (payload: number) => {
     return typeof payload === 'number'
