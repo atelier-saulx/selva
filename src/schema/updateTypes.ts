e771478c12e0ed3f6d15c132654f5248fc9f45c5
Jim de Beer <beerdejim@gmail.com>
2020-01-21 15:46:14 +0100

    make types

--- src/schema/updateTypes.ts
+++ src/schema/updateTypes.ts
@@ -1,7 +1,7 @@
 import { Types, TypesDb } from './'
 
 // small caps counter (2 spaces)
-const uid = num => {
+const uid = (num: number): string => {
   const div = (num / 26) | 0
   var str = String.fromCharCode(97 + (num % 26))
   if (div) {
@@ -19,7 +19,6 @@ const uid = num => {
 //check if - in id else use first 2
 // [flurp-] (this becomes the id)
 // ids cant have - in them!
-
 const findKey = (obj: { [key: string]: any }, value: any): false | string => {
   for (let k in obj) {
     if (obj[k] === value) {
@@ -33,8 +32,16 @@ const validate = id => {
   return /[a-z]{1,10}/.test(id)
 }
 
+const genId = (types: TypesDb): string => {
+  types.idSize++
+  const id = uid(types.idSize)
+  if (findKey(types, id)) {
+    return genId(types)
+  }
+  return id
+}
+
 async function parseTypes(props: Types, types: TypesDb) {
-  // types
   for (let type in props.types) {
     const definition = props.types[type]
     if (!types[type]) {
@@ -53,9 +60,7 @@ async function parseTypes(props: Types, types: TypesDb) {
         }
         types[type] = definition.prefix
       } else {
-        types.idSize++
-        // uid()
-        // generate one your self
+        types[type] = genId(types)
       }
     } else {
       if (definition.prefix !== types[type]) {
