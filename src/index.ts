38b6a9c7418f7a334fff64d40d1ef8ec365ece07
Tony Kovanen <tony.kovanen@gmail.com>
2020-01-22 15:05:56 +0700

    id fixes

--- src/index.ts
+++ src/index.ts
@@ -43,14 +43,11 @@ export class SelvaClient {
 
   async id(props: IdOptions): Promise<string> {
     return this.redis.loadAndEvalScript(
-      'modify',
+      'id',
       ID_SCRIPT,
       0,
       [],
-      [JSON.stringify(props)],
-      {
-        batchingEnabled: true
-      }
+      [JSON.stringify(props)]
     )
   }
 
