5d8be48b2945b5e42df4c448549c4abc656a30e1
Jim de Beer <beerdejim@gmail.com>
2020-01-22 08:57:18 +0100

    field

--- src/set/collectSchemas.ts
+++ src/set/collectSchemas.ts
@@ -57,28 +57,29 @@ const collectSchemas = async (
     throw new Error(`No schema defined for ${payload.type}`)
   }
   for (const key in payload) {
-    if (key[0] !== '$') {
-      if (!schema.fields[key]) {
-        throw new Error(`Field ${key} not in schema ${payload.type}`)
-      } else if (schema.fields[key].type === 'references') {
-        if (Array.isArray(payload[key])) {
-          if (typeof payload[key][0] === 'object') {
-            try {
-              await Promise.all(
-                payload[key].map(child =>
-                  collectSchemas(client, child, schemas, prefixes)
-                )
-              )
-            } catch (err) {
-              throw err
-            }
-          }
-        } else if (payload[key].$add && typeof payload[key].$add === 'object') {
+    if (key[0] === '$') {
+      continue
+    }
+    if (!schema.fields[key]) {
+      throw new Error(`Field ${key} not in schema ${payload.type}`)
+    } else if (schema.fields[key].type === 'references') {
+      if (Array.isArray(payload[key])) {
+        if (typeof payload[key][0] === 'object') {
           try {
-            await collectSchemas(client, payload[key].$add, schemas, prefixes)
+            await Promise.all(
+              payload[key].map(child =>
+                collectSchemas(client, child, schemas, prefixes)
+              )
+            )
           } catch (err) {
             throw err
           }
+        }
+      } else if (payload[key].$add && typeof payload[key].$add === 'object') {
+        try {
+          await collectSchemas(client, payload[key].$add, schemas, prefixes)
+        } catch (err) {
+          throw err
         }
       }
     }
--- src/set/fieldParsers/simple.ts
+++ src/set/fieldParsers/simple.ts
@@ -48,7 +48,7 @@ const verifiers = {
     return typeof payload === 'string' && validURL(payload)
   },
   email: (payload: string) => {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return re.test(payload.toLowerCase())
   },
   number: (payload: number) => {
