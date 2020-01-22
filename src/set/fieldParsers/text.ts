d50e9b2ee6d702d89a81303f353c34f9413d5983
Jim de Beer <beerdejim@gmail.com>
2020-01-22 12:11:27 +0100

    add text

--- src/set/fieldParsers/text.ts
+++ src/set/fieldParsers/text.ts
@@ -1,6 +1,29 @@
 import { SetOptions } from '../types'
 import { TypeSchema, FieldSchemaOther } from '../../schema'
 
+const verify = (payload: SetOptions, nested?: boolean, lang?: string[]) => {
+  for (let key in payload) {
+    if (key === '$merge') {
+      if (nested) {
+        throw new Error(`$merge cannot be used on language fields`)
+      }
+      if (!(payload[key] === false || payload[key] === true)) {
+        throw new Error(`$merge needs to be true or false ${key}`)
+      }
+    } else if (key === '$value' || key === '$default') {
+      verify(payload[key], false, lang)
+    } else if (lang && lang.indexOf(key) !== -1) {
+      if (typeof payload[key] === 'object') {
+        verify(payload[key], true)
+      } else if (typeof payload[key] !== 'string') {
+        throw new Error(`Incorrect type for string ${key}`)
+      }
+    } else {
+      throw new Error(`Incorrect field for text ${key}`)
+    }
+  }
+}
+
 export default (
   schemas: Record<string, TypeSchema>,
   field: string,
@@ -9,6 +32,7 @@ export default (
   fields: FieldSchemaOther,
   type: string
 ): void => {
-  // languages verification
+  const lang: string[] = <string[]>schemas._languages
+  verify(payload[field], false, lang)
   result[field] = payload[field]
 }
