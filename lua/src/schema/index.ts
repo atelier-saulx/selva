f2c0aa1199464e7a569dbfd295146821f9f4b2b7
Tony Kovanen <tony.kovanen@gmail.com>
2020-01-27 15:50:55 +0700

    more sketches

--- lua/src/schema/index.ts
+++ lua/src/schema/index.ts
@@ -1,8 +1,9 @@
 import { Schema } from '../../../src/schema/index'
 import ensurePrefixes from './prefixes'
+import updateSearchIndexes from './searchIndexes'
 import * as r from '../redis'
 
-export function getSchema() {
+export function getSchema(): Schema {
   return cjson.decode(r.get('___selva_schema'))
 }
 
@@ -16,17 +17,52 @@ function verifyLanguages(oldSchema: Schema, newSchema: Schema): string | null {
   return null
 }
 
+const isEqual = (a: any, b: any): boolean => {
+  const typeA = type(a)
+  if (typeA !== type(b)) {
+    return false
+  }
+
+  if (typeA === 'table') {
+    for (let key in a) {
+      if (!b[key]) {
+        return false
+      } else {
+        if (!isEqual(a[key], b[key])) {
+          return false
+        }
+      }
+    }
+  } else if (a !== b) {
+    return false
+  }
+  return true
+}
+
 function verifyTypes(oldSchema: Schema, newSchema: Schema): string | null {
   for (const type in oldSchema.types) {
     if (!newSchema.types[type]) {
       return `New schema definition missing existing type ${type}`
+    }
+
+    if (newSchema.types[type] && oldSchema.types[type]) {
+      // make sure that we're not changing type schemas that already exist
+      // Note: prefix equality is verified in ensurePrefixes()
+      if (
+        !isEqual(oldSchema.types[type].fields, newSchema.types[type].fields)
+      ) {
+        return `Schemas are not the same for type ${type}, trying to change ${cjson.encode(
+          oldSchema.types[type]
+        )} to ${cjson.encode(newSchema.types[type])}`
+      }
+      // Note: hierarchies need not be the same, they can be overwritten
     }
   }
 
   return null
 }
 
-export function verifyAndEnsurePrefixes(
+export function verifyAndEnsureRequiredFields(
   oldSchema: Schema,
   newSchema: Schema
 ): string | null {
@@ -47,12 +83,21 @@ export function verifyAndEnsurePrefixes(
   return null
 }
 
-// TODO: handle hset ___selva_schema, prefixes equivalent
 export function saveSchema(schema: Schema): string {
   const encoded = cjson.encode(schema)
   const sha = redis.sha1hex(encoded)
   schema.sha = sha
   r.set('___selva_schema', encoded) // TODO: is this where we actually want to set it?
   return encoded
+}
+
+// TODO: handle hset ___selva_schema, prefixes equivalent
+// TODO: handle search index changes
+export function updateSchema(newSchema: Schema) {
+  const oldSchema = getSchema()
+  verifyAndEnsureRequiredFields(oldSchema, newSchema)
+  updateSearchIndexes() // TODO
+  // TODO: update ancestors on hierarchy updates
+  saveSchema(newSchema)
 }
 
---
+++ lua/src/schema/searchIndexes.ts
@@ -0,0 +1,4 @@
+export default function updateSearchIndexes(): string | null {
+  // TODO
+  return null
+}
