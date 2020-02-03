170aa75beb1c1f195e46504c3a7d5fca23c1c1e5
Jim de Beer <beerdejim@gmail.com>
2020-02-03 17:11:54 +0700

    fixes

--- src/redis/methods.ts
+++ src/redis/methods.ts
@@ -392,6 +392,12 @@ abstract class RedisMethods {
     })
   }
 
+  async ftTagVals(index: string, tagField: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.queue('FT.TAGVALS', [index, tagField], resolve, reject)
+    })
+  }
+
   // subscriber stuff fix it needs to become better!
   // do it next week
   async psubscribe(...pattern: string[]): Promise<any> {
