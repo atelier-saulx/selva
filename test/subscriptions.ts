efdda55afe3f3088ce3a458dbab95f0e52665161
Tony Kovanen <tony.kovanen@gmail.com>
2020-02-10 17:55:45 +0700

    inherit .ancestors change test

--- test/subscriptions.ts
+++ test/subscriptions.ts
@@ -89,6 +89,7 @@ test.serial('basic id based subscriptions', async t => {
   sub.unsubscribe()
   sub2.unsubscribe()
 
+  await client.delete('root')
   server.destroy()
 })
 
@@ -149,5 +150,72 @@ test.serial('using $field works', async t => {
 
   sub.unsubscribe()
 
+  await client.delete('root')
+  server.destroy()
+})
+
+test.serial('basic $inherit when ancestors change', async t => {
+  const client = connect({ port: 5051 })
+
+  const server = await start({
+    port: 5051,
+    loglevel: 'info',
+    developmentLogging: true
+  })
+
+  await client.updateSchema({
+    languages: ['en', 'de', 'nl'],
+    rootType: {
+      fields: { yesh: { type: 'string' } }
+    },
+    types: {
+      yeshType: {
+        fields: {
+          yesh: { type: 'string' }
+        }
+      }
+    }
+  })
+
+  t.plan(2)
+
+  const thing = await client.set({
+    type: 'yeshType'
+  })
+
+  const observable = await client.observe({
+    $id: thing,
+    id: true,
+    yesh: { $inherit: true }
+  })
+
+  let o1counter = 0
+  const sub = observable.subscribe(d => {
+    if (o1counter === 0) {
+      // gets start event
+      t.deepEqualIgnoreOrder(d, { id: thing, yesh: '' })
+    } else if (o1counter === 1) {
+      // gets update event
+      t.deepEqualIgnoreOrder(d, { id: thing, yesh: 'so nice' })
+    } else {
+      // doesn't get any more events
+      t.fail()
+    }
+    o1counter++
+  })
+
+  await wait(1000 * 1)
+
+  await client.set({
+    type: 'yeshType',
+    yesh: 'so nice',
+    children: [thing]
+  })
+
+  await wait(1000 * 1)
+
+  sub.unsubscribe()
+
+  await client.delete('root')
   server.destroy()
 })
