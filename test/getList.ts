d5afe48c52227366a49ed746f11ef76a01063b06
Jim de Beer <beerdejim@gmail.com>
2020-02-11 11:38:53 +0100

    non redis search

--- test/getList.ts
+++ test/getList.ts
@@ -79,42 +79,37 @@ test.serial('get - simple $list', async t => {
     }
   })
 
-  console.log(c)
-
   await wait(500)
 
-  // t.deepEqual(
-  //   c,
-  //   {
-  //     children: [
-  //       { value: 0, name: 'flurp0' },
-  //       { value: 1, name: 'flurp1' },
-  //       { value: 2, name: 'flurp2' },
-  //       { value: 3, name: 'flurp3' },
-  //       { value: 4, name: 'flurp4' },
-  //       { value: 5, name: 'flurp5' },
-  //       { value: 6, name: 'flurp6' },
-  //       { value: 7, name: 'flurp7' },
-  //       { value: 8, name: 'flurp8' },
-  //       { value: 9, name: 'flurp9' }
-  //     ]
-  //   },
-  //   'use redis search for sort'
-  // )
+  t.deepEqual(
+    c,
+    {
+      children: [
+        { value: 0, name: 'flurp0' },
+        { value: 1, name: 'flurp1' },
+        { value: 2, name: 'flurp2' },
+        { value: 3, name: 'flurp3' },
+        { value: 4, name: 'flurp4' },
+        { value: 5, name: 'flurp5' },
+        { value: 6, name: 'flurp6' },
+        { value: 7, name: 'flurp7' },
+        { value: 8, name: 'flurp8' },
+        { value: 9, name: 'flurp9' }
+      ]
+    },
+    'non redis search sort'
+  )
 
-  // const x = await client.get({
-  //   $id: 'cuA',
-  //   children: {
-  //     name: true,
-  //     value: true,
-  //     $list: {
-  //       $sort: { $field: 'value', $order: 'asc' },
-  //       $range: [0, 10]
-  //     }
-  //   }
-  // })
+  const { children: rangeResult } = await client.get({
+    $id: 'cuA',
+    children: {
+      name: true,
+      value: true,
+      $list: {
+        $range: [0, 10]
+      }
+    }
+  })
 
-  // console.log('flupy')
-
-  t.true(true)
+  t.is(rangeResult.length, 10, 'non redis search range')
 })
