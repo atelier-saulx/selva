4da4d20dd6fa0af91a2a91f768c6afdf7748408c
Jim de Beer <beerdejim@gmail.com>
2020-02-12 15:09:46 +0100

    delete

--- server/lib/backup-plugins/dropbox/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { BackupFns } from '../../backups';
-import { DropboxOptions } from 'dropbox';
-export default function mkBackupFn(opts: DropboxOptions, path: string, rdbFilePath: string): Promise<BackupFns>;
