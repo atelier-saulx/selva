7c8a645dbdcc5ab890926145057071dd92a86ffa
Jim de Beer <beerdejim@gmail.com>
2020-02-18 10:46:09 +0100

    fix

--- server/src/subscriptions/index.ts
+++ server/src/subscriptions/index.ts
@@ -2,116 +2,26 @@ import { createClient, RedisClient } from 'redis'
 import { SelvaClient } from '../../../client/src'
 import { GetOptions } from '../../../client/src/get/types'
 import { QuerySubscription } from '../../../lua/src/get/query/types'
-import { Schema, FieldSchema } from '../../../client/src/schema'
+import { Schema } from '../../../client/src/schema'
 import { createHash } from 'crypto'
-import query from './query'
-import { isObjectLike } from './util'
-
-function makeAll(path: string, schema: Schema, opts: GetOptions): GetOptions {
-  const newOpts: GetOptions = { ...opts }
-  delete newOpts.$all
-
-  const parts = path.split('.')
-  if (!newOpts.$id) {
-    return newOpts
-  }
-
-  const typeName = schema.prefixToTypeMapping[newOpts.$id.substr(0, 2)]
-  const type = schema.types[typeName]
-  if (!type) {
-    return newOpts
-  }
-
-  let prop: FieldSchema = {
-    type: 'object',
-    properties: type.fields
-  }
-
-  for (let i = 0; i < parts.length; i++) {
-    if (!parts[i]) {
-      break
-    }
-
-    if (!isObjectLike(prop)) {
-      break
-    } else {
-      prop = prop.properties[parts[i]]
-    }
-  }
-
-  if (isObjectLike(prop)) {
-    for (const propName in prop.properties) {
-      newOpts[propName] = true
-    }
-  } else if (prop.type === 'text') {
-    for (const lang of schema.languages) {
-      newOpts[lang] = true
-    }
-  }
-
-  return newOpts
-}
-
-function addFields(
-  path: string,
-  fields: Set<string>,
-  schema: Schema,
-  opts: GetOptions
-): void {
-  let hasKeys = false
-  for (const key in opts) {
-    if (key[0] === '$') {
-      if (key === '$all') {
-        addFields(path, fields, schema, makeAll(path, schema, opts))
-        return
-      } else if (key === '$inherit') {
-        fields.add('.ancestors')
-        return
-      } else if (key === '$field') {
-        if (Array.isArray(opts.$field)) {
-          opts.$field.forEach(f => fields.add('.' + f))
-        } else {
-          fields.add('.' + opts.$field)
-        }
-
-        return
-      }
-
-      // FIXME: other special options missing? -- $ref needs to be handled on lua side
-      continue
-    }
-
-    hasKeys = true
-
-    if (opts[key] === true) {
-      fields.add(`${path}.${key}`)
-    } else if (typeof opts[key] === 'object') {
-      addFields(`${path}.${key}`, fields, schema, opts[key])
-    }
-  }
-
-  // default to adding the field if only options are specified
-  if (!hasKeys) {
-    fields.add(path)
-  }
-}
+import addFields from './addFields'
+import attach from './attach'
 
 export default class SubscriptionManager {
-  private refreshSubscriptionsTimeout: NodeJS.Timeout
-  private lastRefreshed: Date
-  private cleanUp: boolean = false
-  private lastModifyEvent: number
+  public refreshSubscriptionsTimeout: NodeJS.Timeout
+  public lastRefreshed: Date
+  public cleanUp: boolean = false
+  public lastModifyEvent: number
   public queries: Record<string, QuerySubscription[]> = {}
   public inProgress: Record<string, true> = {}
-  private subscriptions: Record<string, GetOptions> = {}
-  private subscriptionsByField: Record<string, Set<string>> = {}
-  private refsById: Record<string, Record<string, string>> = {}
-  private lastResultHash: Record<string, string> = {}
-  private lastHeartbeat: Record<string, number> = {}
-
+  public subscriptions: Record<string, GetOptions> = {}
+  public subscriptionsByField: Record<string, Set<string>> = {}
+  public refsById: Record<string, Record<string, string>> = {}
+  public lastResultHash: Record<string, string> = {}
+  public lastHeartbeat: Record<string, number> = {}
   public client: SelvaClient
-  private sub: RedisClient
-  private pub: RedisClient
+  public sub: RedisClient
+  public pub: RedisClient
 
   cleanUpProgress() {
     if (!this.cleanUp) {
@@ -136,166 +46,13 @@ export default class SubscriptionManager {
 
   async attach(port: number) {
     this.client = new SelvaClient({ port }, { loglevel: 'off' })
-
     this.sub = createClient({ port })
     this.pub = createClient({ port })
-
-    this.sub.on('error', e => {
-      // console.error(e)
-    })
-    this.pub.on('error', e => {
-      // console.error(e)
-    })
-
-    let tm: NodeJS.Timeout
     try {
-      await Promise.race([
-        new Promise((resolve, _reject) => {
-          let count = 0
-          this.pub.on('ready', () => {
-            count++
-            if (count === 2) {
-              if (tm) {
-                clearTimeout(tm)
-              }
-
-              resolve()
-            }
-          })
-
-          this.sub.on('ready', () => {
-            count++
-            if (count === 2) {
-              if (tm) {
-                clearTimeout(tm)
-              }
-
-              resolve()
-            }
-          })
-        }),
-        new Promise((_resolve, reject) => {
-          tm = setTimeout(() => {
-            this.pub.removeAllListeners('ready')
-            this.sub.removeAllListeners('ready')
-            reject()
-          }, 5000)
-        })
-      ])
-    } catch (e) {
-      setTimeout(() => {
-        this.attach(port)
-      }, 1000)
+      attach(this, port)
+    } catch (err) {
+      throw new Error(err)
     }
-
-    await this.refreshSubscriptions()
-    this.recalculateUpdates()
-
-    // client heartbeat events
-    this.sub.on('message', (_channel, message) => {
-      const payload: { channel: string; refresh?: boolean } = JSON.parse(
-        message
-      )
-
-      const subId = payload.channel.slice('___selva_subscription:'.length)
-      this.lastHeartbeat[subId] = Date.now()
-
-      if (payload.refresh) {
-        delete this.lastResultHash[subId]
-        this.refreshSubscription(subId)
-          .then(() => {
-            return this.sendUpdate(subId)
-          })
-          .catch(e => {
-            console.error(e)
-          })
-      }
-    })
-
-    // lua object change events
-    this.sub.on('pmessage', async (_pattern, channel, message) => {
-      this.lastModifyEvent = Date.now()
-      if (channel === '___selva_events:heartbeat') {
-        return
-      }
-
-      // used to deduplicate events for subscriptions,
-      // firing only once if multiple fields in subscription are changed
-      const updatedSubscriptions: Record<string, true> = {}
-
-      const eventName = channel.slice('___selva_events:'.length)
-
-      if (message === 'delete') {
-        for (const field in this.subscriptionsByField) {
-          if (field.startsWith(eventName)) {
-            const subscriptionIds: Set<string> | undefined =
-              this.subscriptionsByField[field] || new Set()
-
-            for (const subscriptionId of subscriptionIds) {
-              if (updatedSubscriptions[subscriptionId]) {
-                continue
-              }
-
-              updatedSubscriptions[subscriptionId] = true
-
-              this.sendUpdate(subscriptionId, null, true)
-            }
-          }
-        }
-        return
-      } else if (message === 'update') {
-        const parts = eventName.split('.')
-        let field = parts[0]
-        for (let i = 0; i < parts.length; i++) {
-          const subscriptionIds: Set<string> | undefined =
-            this.subscriptionsByField[field] || new Set()
-
-          for (const subscriptionId of subscriptionIds) {
-            if (updatedSubscriptions[subscriptionId]) {
-              continue
-            }
-
-            updatedSubscriptions[subscriptionId] = true
-
-            this.sendUpdate(subscriptionId).catch(e => {
-              console.error(e)
-            })
-          }
-
-          if (i < parts.length - 1) {
-            field += '.' + parts[i + 1]
-          }
-        }
-      }
-
-      // parses the query part (if it has queries)
-      query(this, message, eventName)
-    })
-
-    this.sub.psubscribe('___selva_events:*')
-    this.sub.subscribe('___selva_subscription:client_heartbeats')
-
-    const timeout = () => {
-      if (Date.now() - this.lastModifyEvent > 1000 * 30) {
-        this.detach()
-        this.attach(port).catch(e => {
-          console.error(e)
-        })
-
-        return
-      }
-
-      this.heartbeats()
-
-      this.refreshSubscriptions()
-        .catch(e => {
-          console.error(e)
-        })
-        .finally(() => {
-          this.refreshSubscriptionsTimeout = setTimeout(timeout, 1000 * 10)
-        })
-    }
-    timeout()
   }
 
   detach() {
@@ -416,7 +173,7 @@ export default class SubscriptionManager {
     )
   }
 
-  private async refreshSubscription(
+  public async refreshSubscription(
     subId: string,
     subs: Record<string, GetOptions> = this.subscriptions,
     fieldMap: Record<string, Set<string>> = this.subscriptionsByField,
@@ -469,7 +226,7 @@ export default class SubscriptionManager {
     }
   }
 
-  private recalculateUpdates() {
+  public recalculateUpdates() {
     for (const subId in this.subscriptions) {
       this.sendUpdate(subId).catch(e => {
         console.error(e)
@@ -477,7 +234,7 @@ export default class SubscriptionManager {
     }
   }
 
-  private async refreshSubscriptions() {
+  public async refreshSubscriptions() {
     const schema = (await this.client.getSchema()).schema
 
     const lastEdited = await this.client.redis.hget(
