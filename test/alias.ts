7809fef9fde2fbc8f8a22568ca26e041ce53ad76
Jim de Beer <beerdejim@gmail.com>
2020-02-20 12:00:05 +0100

    fix

---
+++ test/alias.ts
@@ -0,0 +1,130 @@
+import test from 'ava'
+import { connect } from '../client/src/index'
+import { start } from '../server/src/index'
+import './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  await new Promise((resolve, _reject) => {
+    setTimeout(resolve, 100)
+  })
+
+  const client = connect({ port }, { loglevel: 'info' })
+  await client.updateSchema({
+    languages: ['en'],
+    types: {
+      lekkerType: {
+        prefix: 'vi',
+        fields: {
+          thing: { type: 'set', items: { type: 'string' } },
+          ding: {
+            type: 'object',
+            properties: {
+              dong: { type: 'set', items: { type: 'string' } },
+              dung: { type: 'number' },
+              dang: {
+                type: 'object',
+                properties: {
+                  dung: { type: 'number' },
+                  dunk: { type: 'string' }
+                }
+              },
+              dunk: {
+                type: 'object',
+                properties: {
+                  ding: { type: 'number' },
+                  dong: { type: 'number' }
+                }
+              }
+            }
+          },
+          dong: { type: 'json' },
+          dingdongs: { type: 'array', items: { type: 'string' } },
+          refs: { type: 'references' },
+          value: { type: 'number' },
+          age: { type: 'number' },
+          auth: {
+            type: 'json'
+          },
+          title: { type: 'text' },
+          description: { type: 'text' },
+          image: {
+            type: 'object',
+            properties: {
+              thumb: { type: 'string' },
+              poster: { type: 'string' }
+            }
+          }
+        }
+      },
+      custom: {
+        prefix: 'cu',
+        fields: {
+          value: { type: 'number' },
+          age: { type: 'number' },
+          auth: {
+            type: 'json'
+          },
+          title: { type: 'text' },
+          description: { type: 'text' },
+          image: {
+            type: 'object',
+            properties: {
+              thumb: { type: 'string' },
+              poster: { type: 'string' }
+            }
+          }
+        }
+      },
+      club: {
+        prefix: 'cl',
+        fields: {
+          value: { type: 'number' },
+          age: { type: 'number' },
+          auth: {
+            type: 'json'
+          },
+          title: { type: 'text' },
+          description: { type: 'text' },
+          image: {
+            type: 'object',
+            properties: {
+              thumb: { type: 'string' },
+              poster: { type: 'string' }
+            }
+          }
+        }
+      },
+      match: {
+        prefix: 'ma',
+        fields: {
+          title: { type: 'text' },
+          description: { type: 'text' }
+        }
+      }
+    }
+  })
+
+  await client.destroy()
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  await client.delete('root')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('get $value', async t => {
+  const client = connect({ port })
+
+  await client.delete('root')
+  client.destroy()
+})
