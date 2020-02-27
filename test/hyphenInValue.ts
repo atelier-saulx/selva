6da41e2fcbf0998dd7dba960692e5b5a9fd884f9
Nuno Frade <nfrade.tribal@gmail.com>
2020-02-27 10:29:07 +0100

    Adds failing test to find values with hyphen

---
+++ test/hyphenInValue.ts
@@ -0,0 +1,128 @@
+import test from 'ava'
+import { connect } from '../client/src/index'
+import { start } from '../server/src/index'
+import './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  await new Promise((resolve, _reject) => {
+    setTimeout(resolve, 100)
+  })
+
+  const client = connect({ port }, { loglevel: 'info' })
+  await client.updateSchema({
+    // languages: ['en'],
+    types: {
+      actor: {
+        prefix: 'ac',
+        fields: {
+          name: { type: 'string', search: { type: ['TAG'] } },
+          born: { type: 'int' },
+          died: { type: 'int' }
+        }
+      }
+    }
+  })
+
+  await client.destroy()
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  await client.delete('root')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('get value without hyphen', async t => {
+  const client = connect({ port })
+
+  await Promise.all([
+    {
+      name: 'Charlton Heston',
+      born: 1923,
+      died: 2008
+    },
+    {
+      name: 'Leigh TaylorYoung',
+      born: 1945
+    }
+  ].map((actor) => client.set({
+    type: 'actor',
+    ...actor
+  })))
+
+  t.deepEqualIgnoreOrder(
+    await client.get({
+    items: {
+      name: true,
+      $list: {
+        $find: {
+          $traverse: 'children',
+          $filter: [
+            { $field: 'type', $operator: '=', $value: 'actor' },
+            { $field: 'name', $operator: '=', $value: 'Leigh TaylorYoung' }
+          ]
+        }
+      }
+    }
+  }),
+    {
+      items: [{ name: 'Leigh TaylorYoung' }]
+    }
+  )
+  t.pass()
+
+  await client.delete('root')
+  client.destroy()
+})
+
+test.serial('get value with hyphen', async t => {
+  const client = connect({ port })
+
+  await Promise.all([
+    {
+      name: 'Charlton Heston',
+      born: 1923,
+      died: 2008
+    },
+    {
+      name: 'Leigh Taylor-Young',
+      born: 1945
+    }
+  ].map((actor) => client.set({
+    type: 'actor',
+    ...actor
+  })))
+
+  t.deepEqualIgnoreOrder(
+    await client.get({
+    items: {
+      name: true,
+      $list: {
+        $find: {
+          $traverse: 'children',
+          $filter: [
+            { $field: 'type', $operator: '=', $value: 'actor' },
+            { $field: 'name', $operator: '=', $value: 'Leigh Taylor-Young' }
+          ]
+        }
+      }
+    }
+  }),
+    {
+      items: [{ name: 'Leigh Taylor-Young' }]
+    }
+  )
+  t.pass()
+
+  await client.delete('root')
+  client.destroy()
+})
