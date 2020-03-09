26379cd0a993be8e610a3566be0f03ca54890002
Tony Kovanen <tony.kovanen@gmail.com>
2020-03-09 16:40:38 +0100

    extend conformToSchema to arrays and sets

--- client/src/index.ts
+++ client/src/index.ts
@@ -88,7 +88,7 @@ export class SelvaClient {
   }
 
   async conformToSchema(props: SetOptions): Promise<SetOptions> {
-    if (!props.$id || props.type || props.$alias) {
+    if (!props.$id && !props.type && !props.$alias) {
       return null
     }
 
@@ -112,13 +112,16 @@ export class SelvaClient {
     }
 
     const typeSchema =
-      props.$type === 'root'
+      props.type === 'root'
         ? this.schema.rootType
         : this.schema.types[props.type]
 
     const newProps: SetOptions = {
-      $id: props.$id,
       type: props.type
+    }
+
+    if (props.$id) {
+      newProps.$id = props.$id
     }
 
     if (props.$alias) {
@@ -137,6 +140,24 @@ export class SelvaClient {
               oldObj[key],
               <FieldSchemaObject>schema.properties[key]
             )
+          } else if (
+            schema.properties[key].type === 'array' &&
+            // @ts-ignore
+            schema.properties[key].items.type === 'object'
+          ) {
+            newObj[key] = oldObj[key].map(x => {
+              // @ts-ignore
+              return mergeObject(x, schema.properties[key].items)
+            })
+          } else if (
+            schema.properties[key].type === 'set' &&
+            // @ts-ignore
+            schema.properties[key].items.type === 'object'
+          ) {
+            newObj[key] = oldObj[key].map(x => {
+              // @ts-ignore
+              return mergeObject(x, schema.properties[key].items)
+            })
           } else {
             newObj[key] = oldObj[key]
           }
@@ -153,6 +174,24 @@ export class SelvaClient {
             props[key],
             <FieldSchemaObject>typeSchema.fields[key]
           )
+        } else if (
+          typeSchema.fields[key].type === 'array' &&
+          // @ts-ignore
+          typeSchema.fields[key].items.type === 'object'
+        ) {
+          newProps[key] = props[key].map(x => {
+            // @ts-ignore
+            return mergeObject(x, typeSchema.fields[key].items)
+          })
+        } else if (
+          typeSchema.fields[key].type === 'set' &&
+          // @ts-ignore
+          typeSchema.fields[key].items.type === 'object'
+        ) {
+          newProps[key] = props[key].map(x => {
+            // @ts-ignore
+            return mergeObject(x, typeSchema.fields[key].items)
+          })
         } else {
           newProps[key] = props[key]
         }
