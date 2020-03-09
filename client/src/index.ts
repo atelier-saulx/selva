ef848b607e4b6c252aa131881b6837ba9a55e039
Tony Kovanen <tony.kovanen@gmail.com>
2020-03-09 16:12:27 +0100

    add conformToSchema helper to client

--- client/src/index.ts
+++ client/src/index.ts
@@ -7,7 +7,13 @@ import { get, GetOptions, GetResult } from './get'
 import { observe } from './observe/index'
 import { readFileSync } from 'fs'
 import { join as pathJoin } from 'path'
-import { Schema, SearchIndexes, SchemaOptions, Id } from './schema'
+import {
+  Schema,
+  SearchIndexes,
+  SchemaOptions,
+  Id,
+  FieldSchemaObject
+} from './schema'
 import { newSchemaDefinition } from './schema/updateSchema'
 import { getSchema } from './schema/getSchema'
 import getTypeFromId from './getTypeFromId'
@@ -79,6 +85,81 @@ export class SelvaClient {
     if (selvaOpts && selvaOpts.loglevel) {
       this.loglevel = selvaOpts.loglevel
     }
+  }
+
+  async conformToSchema(props: SetOptions): Promise<SetOptions> {
+    if (!props.$id || props.type || props.$alias) {
+      return null
+    }
+
+    if (!props.type) {
+      if (props.$id) {
+        props.type = await getTypeFromId(this, props.$id)
+      } else {
+        const typePayload = await this.get({
+          $alias: props.$alias,
+          type: true,
+          id: true
+        })
+
+        props.type = typePayload.type
+        props.$id = typePayload.id
+      }
+    }
+
+    if (!props.type) {
+      return null
+    }
+
+    const typeSchema =
+      props.$type === 'root'
+        ? this.schema.rootType
+        : this.schema.types[props.type]
+
+    const newProps: SetOptions = {
+      $id: props.$id,
+      type: props.type
+    }
+
+    if (props.$alias) {
+      newProps.$alias = props.$alias
+    }
+
+    const mergeObject: (
+      x: SetOptions,
+      schema: FieldSchemaObject
+    ) => SetOptions = (oldObj: SetOptions, schema: FieldSchemaObject) => {
+      const newObj: SetOptions = {}
+      for (const key in oldObj) {
+        if (schema.properties[key]) {
+          if (schema.properties[key].type === 'object') {
+            newObj[key] = mergeObject(
+              oldObj[key],
+              <FieldSchemaObject>schema.properties[key]
+            )
+          } else {
+            newObj[key] = oldObj[key]
+          }
+        }
+      }
+
+      return newObj
+    }
+
+    for (const key in props) {
+      if (typeSchema.fields[key]) {
+        if (typeSchema.fields[key].type === 'object') {
+          newProps[key] = mergeObject(
+            props[key],
+            <FieldSchemaObject>typeSchema.fields[key]
+          )
+        } else {
+          newProps[key] = props[key]
+        }
+      }
+    }
+
+    return newProps
   }
 
   digest(payload: string) {
