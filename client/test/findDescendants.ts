c2849e94449d6b6749a12377ba8fa8678d4c3190
Jim de Beer <beerdejim@gmail.com>
2020-03-10 15:48:49 +0100

    fix

--- client/test/findDescendants.ts
+++ client/test/findDescendants.ts
@@ -139,12 +139,12 @@ test.before(async t => {
 })
 
 test.after(async _t => {
-  // const client = connect({ port })
-  // const d = Date.now()
-  // await client.delete('root')
-  // console.log('removed', Date.now() - d, 'ms')
-  // await client.destroy()
-  // await srv.destroy()
+  const client = connect({ port })
+  const d = Date.now()
+  await client.delete('root')
+  console.log('removed', Date.now() - d, 'ms')
+  await client.destroy()
+  await srv.destroy()
 })
 
 test.serial('find - descendants', async t => {
