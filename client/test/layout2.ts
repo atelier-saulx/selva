e9a02e10714c53abbe7593654d9a407c7463d82e
youzi <youridaamen@gmail.com>
2020-04-15 14:48:21 +0200

    reuse same test

--- client/test/layout2.ts
+++ client/test/layout2.ts
@@ -101,7 +101,7 @@ test.after(async _t => {
   await srv.destroy()
 })
 
-test.serial('layout without inheritance or refs', async t => {
+const layoutSubsTest = async (t, layoutSub) => {
   const client = connect({ port })
   // creating data
   const match = await client.set({
@@ -141,7 +141,7 @@ test.serial('layout without inheritance or refs', async t => {
   client
     .observe({
       $id: match,
-      layout: true //{ $inherit: true } // change this to { $inherit: true } to break it
+      layout: layoutSub // one test passes true / other passes { $inherit: true }
     })
     .then(layoutObs =>
       layoutObs.subscribe(async res => {
@@ -237,9 +237,9 @@ test.serial('layout without inheritance or refs', async t => {
       t.is(description, `description-${updateCount}`)
     })
   }
-})
+}
 
-test.serial('layout with refs', async t => {
+const layoutWithRefsTest = async t => {
   const client = connect({ port })
   // add some refs to the layout!
   const dictionary = await client.set({
@@ -330,142 +330,10 @@ test.serial('layout with refs', async t => {
     t.is(totalCount, fireCount)
     t.is(layout.components[0].title, `dictionary-${dictionaryCount}`)
   }
-})
+}
 
-test.serial('layout using $inherit: true - own layout', async t => {
-  const client = connect({ port })
-  // creating data
-  const match = await client.set({
-    type: 'match',
-    $language: 'en',
-    title: 'title-0',
-    description: 'description-0',
-    start: Date.now() - 10000,
-    end: Date.now() + 60 * 60 * 1000 * 2,
-    layout: {
-      match: {
-        id: true,
-        components: [
-          {
-            component: { $value: 'comp1-0' },
-            title: {
-              $field: 'title'
-            },
-            description: {
-              $field: 'description'
-            }
-          }
-        ]
-      }
-    }
-  })
-
-  // subscribe to layout of match, and update match page subscription
-  let updateCount = 0
-  let totalCount = 0
-  let fireCount = 0
-  let pageObs
-  let pageSub
-  let layout
-  let query
-
-  client
-    .observe({
-      $id: match,
-      layout: { $inherit: true }
-    })
-    .then(layoutObs =>
-      layoutObs.subscribe(async res => {
-        if (pageSub) {
-          pageSub.unsubscribe()
-        }
-        query = {
-          $id: match,
-          $language: 'en',
-          ...res.layout.match
-        }
-        pageObs = await client.observe(query)
-        pageSub = pageObs.subscribe(res => {
-          layout = res
-          fireCount++
-        })
-      })
-    )
-
-  while (updateCount <= 2) {
-    updateCount++
-    totalCount++
-    await client.set({
-      $id: match,
-      layout: {
-        match: {
-          components: [
-            {
-              component: {
-                $value: `comp1-${updateCount}`
-              },
-              title: {
-                $field: 'title'
-              },
-              description: {
-                $field: 'description'
-              }
-            }
-          ]
-        }
-      }
-    })
-    await wait(100)
-    t.is(totalCount, fireCount)
-    t.is(layout.components[0].component, `comp1-${updateCount}`)
-  }
-
-  while (updateCount <= 4) {
-    updateCount++
-    totalCount++
-    await client.set({
-      $id: match,
-      $language: 'en',
-      title: `title-${updateCount}`,
-      description: `description-${updateCount}`
-    })
-    await wait(100)
-    t.is(totalCount, fireCount)
-    t.is(layout.components[0].title, `title-${updateCount}`)
-    t.is(layout.components[0].description, `description-${updateCount}`)
-  }
-
-  let compCount = 1
-  while (compCount <= 2) {
-    compCount++
-    totalCount++
-    await client.set({
-      $id: match,
-      layout: {
-        match: {
-          components: [
-            ...query.components,
-            {
-              component: {
-                $value: `comp${compCount}-${updateCount}`
-              },
-              title: {
-                $field: 'title'
-              },
-              description: {
-                $field: 'description'
-              }
-            }
-          ]
-        }
-      }
-    })
-    await wait(100)
-    t.is(totalCount, fireCount)
-    t.is(layout.components.length, compCount)
-    layout.components.forEach(({ title, description }) => {
-      t.is(title, `title-${updateCount}`)
-      t.is(description, `description-${updateCount}`)
-    })
-  }
-})
+test.serial('layout with refs', layoutWithRefsTest)
+test.serial('layout without inheritance or refs', t => layoutSubsTest(t, true))
+test.serial('layout using $inherit: true - own layout, no refs', t =>
+  layoutSubsTest(t, { $inherit: true })
+)
