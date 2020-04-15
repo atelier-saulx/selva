a2df3729f1ac6491149de5b74c38519127762afb
Jim de Beer <beerdejim@gmail.com>
2020-04-15 10:48:41 +0200

    inhrit tests

--- client/test/inherit.ts
+++ client/test/inherit.ts
@@ -20,7 +20,14 @@ test.before(async t => {
         prefix: 'ge',
         fields: {
           name: { type: 'string' },
-          icon: { type: 'string' }
+          icon: { type: 'string' },
+          fields: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              something: { type: 'string' }
+            }
+          }
         }
       },
       movie: {
@@ -41,7 +48,7 @@ test.after(async _t => {
   await srv.destroy()
 })
 
-test.serial('$list', async t => {
+test.serial('simple', async t => {
   const client = connect({ port: port })
 
   const genre = await client.set({
@@ -60,7 +67,121 @@ test.serial('$list', async t => {
     $id: 'moSoylentGreen',
     icon: { $inherit: true }
   })
-  console.log(result)
 
   t.true(result.icon === 'scifi.png')
+})
+
+test.serial('$all', async t => {
+  const client = connect({ port: port })
+
+  const genre = await client.set({
+    $id: 'geA',
+    fields: {
+      name: 'hello'
+    }
+  })
+
+  const genre2 = await client.set({
+    $id: 'geB',
+    parents: ['geA']
+  })
+
+  const result1 = await client.get({
+    $id: 'geA',
+    fields: {
+      $all: true,
+      $inherit: true
+    }
+  })
+
+  t.deepEqual(result1.fields, { name: 'hello', something: '' }, 'from field')
+
+  const result = await client.get({
+    $id: 'geB',
+    fields: {
+      $all: true,
+      $inherit: true
+    }
+  })
+
+  console.log(result.fields)
+  t.deepEqual(result.fields, { name: 'hello', something: '' }, 'inherit')
+})
+
+test.serial('$field + object', async t => {
+  const client = connect({ port: port })
+
+  const genre = await client.set({
+    $id: 'geC',
+    fields: {
+      name: 'hello'
+    }
+  })
+
+  const genre2 = await client.set({
+    $id: 'geD',
+    parents: ['geA']
+  })
+
+  const result1 = await client.get({
+    $id: 'geC',
+    flaprdol: {
+      name: { $field: 'fields.name' },
+      $inherit: true
+    }
+  })
+
+  t.deepEqual(result1, { flaprdol: { name: 'hello' } }, 'get')
+
+  const result = await client.get({
+    $id: 'geD',
+    flaprdol: {
+      name: { $field: 'fields.name' },
+      $inherit: true
+    }
+  })
+
+  console.log('res', result)
+
+  t.deepEqual(result, { flaprdol: { name: 'hello' } }, 'inherit')
+})
+
+test.serial('$field + object + all', async t => {
+  const client = connect({ port: port })
+
+  const genre = await client.set({
+    $id: 'geC',
+    fields: {
+      name: 'hello'
+    }
+  })
+
+  const genre2 = await client.set({
+    $id: 'geD',
+    parents: ['geA']
+  })
+
+  const result1 = await client.get({
+    $id: 'geC',
+    flaprdol: {
+      $all: true,
+      $field: 'fields',
+      $inherit: true
+    }
+  })
+
+  t.deepEqual(result1, { flaprdol: { name: 'hello', something: '' } }, 'get')
+
+  const result = await client.get({
+    $id: 'geD',
+    flaprdol: {
+      $all: true,
+      $field: 'fields',
+      $inherit: true
+    }
+  })
+
+  console.log('res', result)
+
+  t.deepEqual(result, { flaprdol: { name: 'hello', something: '' } }, 'inherit')
 })
