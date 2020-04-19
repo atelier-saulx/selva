e2ef9174d5890a1876c04cc36751879f61edf7a5
youzi <youridaamen@gmail.com>
2020-04-19 10:33:42 +0200

    added test for $language bug in nested text props

---
+++ client/test/z2.ts
@@ -0,0 +1,61 @@
+import test from 'ava'
+import { connect } from '../src/index'
+import { start } from '@saulx/selva-server'
+import './assertions'
+import { wait } from './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  const client = connect({ port })
+  await client.updateSchema({
+    languages: ['en'],
+    types: {
+      dictionary: {
+        prefix: 'di',
+        fields: {
+          words: {
+            type: 'object',
+            properties: {
+              rando: { type: 'text' }
+            }
+          }
+        }
+      }
+    }
+  })
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  const d = Date.now()
+  await client.delete('root')
+  console.log('removed', Date.now() - d, 'ms')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('$language should be applied in nested text', async t => {
+  const client = connect({ port }, { loglevel: 'info' })
+  const dictionary = await client.set({
+    $language: 'en',
+    type: 'dictionary',
+    words: {
+      rando: 'my word'
+    }
+  })
+
+  t.deepEqual(
+    await client.get({
+      $id: dictionary,
+      $language: 'en',
+      words: true
+    }),
+    { words: { rando: 'my word' } }
+  )
+})
