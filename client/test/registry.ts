041263405bb20bbdd7535ea00712d7a5a67df912
Jim de Beer <beerdejim@gmail.com>
2020-05-04 13:17:58 +0200

    start

---
+++ client/test/registry.ts
@@ -0,0 +1,28 @@
+import test from 'ava'
+// import { connect } from '../src/index'
+// import { start } from '@saulx/selva-server'
+// import './assertions'
+// import { wait } from './assertions'
+
+// let srv
+
+console.log('crazy time')
+
+// connect
+
+// simple redis functions e.g. hget etc
+
+// origin
+// cache
+// replica
+// registry
+
+// subscribe handler
+// make subs a bit cleaner
+
+// redis  manages a quue for dc
+// redis-client manages buffer and reconn queues
+
+// redis-client allways has a subscriber and publisher client
+
+// if connecting to cache (used for subscriptions) handle things alittle bit different in 'redis'
