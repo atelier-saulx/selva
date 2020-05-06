7b92b95777c6c517aa5e3f4c92a724e3acddd6dd
Jim de Beer <beerdejim@gmail.com>
2020-05-06 16:26:24 +0200

    index

--- client/src/redis/clients/index.ts
+++ client/src/redis/clients/index.ts
@@ -1,13 +1,15 @@
 import { RedisClient } from 'redis'
 import { RedisCommand, Client } from '../types'
 import './redisSearch'
+
 // getClient
 //   addSelvaClient
+
+// --- in coming add command queue ---
 
 // handle these special
 // subscribe
 // unsubscribe
-
 // psubscribe
 // punsubcribe
 
