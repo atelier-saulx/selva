71c5e8de7c5a1cb0c99587e94bfc135f9ab708c4
youzi <youri@saulx.com>
2020-05-08 09:27:26 +0200

    z5 test double schema subscribe

---
+++ client/test/z5.ts
@@ -0,0 +1,55 @@
+import test from 'ava'
+import { connect } from '../src/index'
+import { start } from '@saulx/selva-server'
+import './assertions'
+import { wait } from './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  const client = connect({ port })
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  const d = Date.now()
+  await client.delete('root')
+  console.log('removed', Date.now() - d, 'ms')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('yes', async t => {
+  const client = connect({ port }, { loglevel: 'info' })
+  const obs = client.subscribeSchema()
+
+  await client.updateSchema({
+    languages: ['en'],
+    rootType: {
+      fields: {
+        title: { type: 'text' }
+      }
+    }
+  })
+
+  await wait(500)
+  let cnt = 0
+  obs.subscribe(schema => {
+    cnt++
+  })
+
+  await wait(500)
+
+  obs.subscribe(schema => {
+    cnt++
+  })
+
+  await wait(500)
+
+  t.is(cnt, 2)
+})
