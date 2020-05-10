806593c51d8e2395bb95352db2c1acb4c8d54958
Jim de Beer <beerdejim@gmail.com>
2020-05-10 15:06:04 +0200

    fix

--- client/src/redis/types.ts
+++ client/src/redis/types.ts
@@ -15,5 +15,6 @@ export type RedisCommand = Resolvable & {
 export type Type = {
   name?: string
   type?: ServerType
-  id?: string // url:port for specifics e.g. a single replica, a single subscription manager
+  host?: string
+  port?: number
 }
--- server/src/server/updateRegistry.ts
+++ server/src/server/updateRegistry.ts
@@ -3,7 +3,29 @@ import { RegistryInfo } from '../types'
 
 export default async function updateRegistry(
   client: SelvaClient,
-  info: RegistryInfo
+  info: RegistryInfo,
+  host: string,
+  port: number
 ) {
-  console.log('hello', info)
+  console.log('write to registry', info)
+
+  const args = []
+
+  for (let key in info) {
+    if (key === 'stats') {
+      args.push(key, JSON.stringify(info[key]))
+    } else if (key === 'subscriptions') {
+      args.push(key, JSON.stringify([...info[key].values()]))
+    } else {
+      args.push(key, JSON.stringify(info[key]))
+    }
+  }
+
+  return client.redis.hmset(
+    {
+      name: 'registry',
+      type: 'registry'
+    },
+    ...args
+  )
 }
