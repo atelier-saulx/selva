637b7754ee44534a920f66f1a992c41af88dee5b
Jim de Beer <beerdejim@gmail.com>
2020-05-22 15:22:18 +0200

    flapflap

--- server/modules/selva/module/module.c
+++ server/modules/selva/module/module.c
@@ -7,10 +7,8 @@
 
 char *genUuid(char *uuid_str) {
   uuid_t uuid;
-
   uuid_generate(uuid);
   uuid_unparse_lower(uuid, uuid_str);
-
   return uuid_str;
 }
 
@@ -20,8 +18,6 @@ int hash(char *hash_str, char *str, size_t strlen) {
   while (i) {
     hash = (hash * 33) ^ (int)(str[--i]);
   }
-
-
   return sprintf(hash_str, "%x", (unsigned int)hash >> 0);
 }
 
@@ -29,47 +25,52 @@ int GenId(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   // init auto memory for created strings
   RedisModule_AutoMemory(ctx);
 
-  // if (argc > 2) {
-  //   return RedisModule_WrongArity(ctx);
-  // }
+  if (argc > 2) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  if (argc == 2) {
+    const char *uuid_str = RedisModule_StringPtrLen(argv[1], NULL);
+    char hash_str[37];
+    hash(hash_str, uuid_str, strlen(uuid_str));
+    RedisModuleString *reply =
+        RedisModule_CreateString(ctx, hash_str, strlen(hash_str) * sizeof(char));
+    RedisModule_ReplyWithString(ctx, reply);
+    return REDISMODULE_OK;
+  } else {
+    char uuid_str[37];
+    genUuid(uuid_str);
+    char hash_str[37];
+    hash(hash_str, uuid_str, 37);
+    RedisModuleString *reply =
+        RedisModule_CreateString(ctx, hash_str, strlen(hash_str) * sizeof(char));
+    RedisModule_ReplyWithString(ctx, reply);
+    return REDISMODULE_OK;
+  }
+}
+
+
+int Flurpy(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+
 
   RedisModuleString *keyStr = RedisModule_CreateString(ctx, "flurpypants", strlen("flurpypants") * sizeof(char));
   RedisModuleString *val = RedisModule_CreateString(ctx, "hallo", strlen("hallo") * sizeof(char));
-
   RedisModuleKey *key = RedisModule_OpenKey(ctx, keyStr, REDISMODULE_WRITE);
   for (int i = 0; i < 10000; i++) {
     RedisModule_StringSet(key, val);
-    RedisModuleCallReply *r = RedisModule_Call(ctx, "publish", "x", "y");
+    // RedisModuleCallReply *r = RedisModule_Call(ctx, "publish", "x", "y");
   }
 
   RedisModule_CloseKey(key);
-
-
   RedisModuleString *reply =
     RedisModule_CreateString(ctx, "hallo", strlen("hallo") * sizeof(char));
-
-  // if (argc == 2) {
-  //   const char *uuid_str = RedisModule_StringPtrLen(argv[1], NULL);
-  //   char hash_str[37];
-  //   hash(hash_str, uuid_str, strlen(uuid_str));
-  //   RedisModuleString *reply =
-  //       RedisModule_CreateString(ctx, hash_str, strlen(hash_str) * sizeof(char));
-  //   RedisModule_ReplyWithString(ctx, reply);
-  //   return REDISMODULE_OK;
-  // } else {
-  //   char uuid_str[37];
-  //   genUuid(uuid_str);
-  //   char hash_str[37];
-  //   hash(hash_str, uuid_str, 37);
-  //   RedisModuleString *reply =
-  //       RedisModule_CreateString(ctx, hash_str, strlen(hash_str) * sizeof(char));
-  //   RedisModule_ReplyWithString(ctx, reply);
-  //   return REDISMODULE_OK;
-  // }
   RedisModule_ReplyWithString(ctx, reply);
   return REDISMODULE_OK;
 
 }
+
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx) {
 
@@ -79,6 +80,10 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
   }
 
   if (RedisModule_CreateCommand(ctx, "selva.id", GenId, "readonly", 1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "selva.flurpy", Flurpy, "readonly", 1, 1, 1) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 
