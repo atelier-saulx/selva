66118968380570516a4120e5d0df7ddbd48f2cce
Tony Kovanen <tony.kovanen@gmail.com>
2020-05-23 20:30:02 +0200

    sketches

---
+++ server/modules/selva/module/modify/modify.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#include "./modify.h"
+
+#define CLIENT_SOCK_FILE "/tmp/selva.sock"
+
+int fd = -1;
+struct sockaddr_un addr;
+
+int SelvaModify_SendAsyncTask(enum SelvaModify_AsyncTask async_task_type, int payload_length, char *payload, uint8_t retries) {
+  if (fd != -1) {
+    fd = socket(PF_UNIX, SOCK_DGRAM, 0);
+    if (fd < 0) {
+      fd = -1;
+      goto error;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strcpy(addr.sun_path, CLIENT_SOCK_FILE);
+    unlink(CLIENT_SOCK_FILE);
+
+
+    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+      goto error;
+    }
+  }
+
+  char buf[1024];
+  sprintf(buf, "%c%s\n", async_task_type, payload);
+  if (write(fd, buf, 1 + payload_length + 1) != 0) {
+    goto error;
+  }
+
+  return 0;
+
+error:
+  if (fd > 0) {
+    close(fd);
+  }
+
+  fd = -1;
+
+  fprintf(stderr, "Error opening unix domain socket\n");
+  if (retries > 5) {
+    fprintf(stderr, "Retries exceeded\n");
+    exit(1);
+  }
+
+  return SelvaModify_SendAsyncTask(async_task_type, payload_length, payload, ++retries);
+}
--- server/modules/selva/module/modify/modify.h
+++ server/modules/selva/module/modify/modify.h
@@ -7,11 +7,18 @@ enum SelvaModify_ArgType {
   SELVA_MODIFY_ARG_BASIC = '1'
 };
 
+enum SelvaModify_AsyncTask {
+  SELVA_MODIFY_ASYNC_TASK_PUBLISH = '0',
+  SELVA_MODIFY_ASYNC_TASK_INDEX = '1',
+};
+
 struct SelvaModify_Basic {
   char *$default;
   char *$value;
   char *$ref;
   char *$increment;
 };
+
+int SelvaModify_SendAsyncTask(enum SelvaModify_AsyncTask async_task_type, int payload_length, char *payload, uint8_t retries);
 
 #endif /* SELVA_MODIFY */
