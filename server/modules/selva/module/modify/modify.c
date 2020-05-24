76082a6f5793f873f796bcfe5fc2cde729920fbc
Tony Kovanen <tony.kovanen@gmail.com>
2020-05-24 10:50:51 +0200

    no unlink

--- server/modules/selva/module/modify/modify.c
+++ server/modules/selva/module/modify/modify.c
@@ -28,7 +28,6 @@ int SelvaModify_SendAsyncTask(int payload_size, char *payload, uint8_t retries) 
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, CLIENT_SOCK_FILE, sizeof(addr.sun_path)-1);
-    unlink(CLIENT_SOCK_FILE);
 
 
     if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
