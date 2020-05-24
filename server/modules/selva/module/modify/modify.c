ec004d8ed8ca08fbc0b9aacec8a8d3e7f1d31647
Tony Kovanen <tony.kovanen@gmail.com>
2020-05-24 10:04:57 +0200

    fixes

--- server/modules/selva/module/modify/modify.c
+++ server/modules/selva/module/modify/modify.c
@@ -14,7 +14,7 @@ int fd = -1;
 struct sockaddr_un addr;
 
 int SelvaModify_SendAsyncTask(enum SelvaModify_AsyncTask async_task_type, int payload_size, char *payload, uint8_t retries) {
-  if (fd != -1) {
+  if (fd == -1) {
     fd = socket(PF_UNIX, SOCK_DGRAM, 0);
     if (fd < 0) {
       fd = -1;
@@ -48,10 +48,10 @@ error:
   fd = -1;
 
   fprintf(stderr, "Error opening unix domain socket\n");
-  if (retries > 5) {
+  if (retries <= 0) {
     fprintf(stderr, "Retries exceeded\n");
     exit(1);
   }
 
-  return SelvaModify_SendAsyncTask(async_task_type, payload_size, payload, ++retries);
+  return SelvaModify_SendAsyncTask(async_task_type, payload_size, payload, --retries);
 }
