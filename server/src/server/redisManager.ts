3c58deece96c3c703f01dbf26a6f2fff343c7ad8
Jim de Beer <beerdejim@gmail.com>
2020-05-25 12:05:48 +0200

    try catch info

--- server/src/server/redisManager.ts
+++ server/src/server/redisManager.ts
@@ -13,25 +13,28 @@ export default class RedisManager extends ProcessManager {
   protected async collect(): Promise<any> {
     const runtimeInfo = await super.collect()
 
-    const info = await promisify(exec)(`redis-cli -p ${this.redisPort} INFO`)
-    const infoLines = info.stdout.split('\r\n')
-    const redisInfo = infoLines.reduce((acc, line) => {
-      if (line.startsWith('#')) {
-        return acc
-      }
+    try {
+      const info = await promisify(exec)(`redis-cli -p ${this.redisPort} INFO`)
+      const infoLines = info.stdout.split('\r\n')
+      const redisInfo = infoLines.reduce((acc, line) => {
+        if (line.startsWith('#')) {
+          return acc
+        }
 
-      const [key, val] = line.split(':')
-      if (key === '') {
-        return acc
-      }
+        const [key, val] = line.split(':')
+        if (key === '') {
+          return acc
+        }
 
-      return {
-        ...acc,
-        [key]: val
-      }
-    }, {})
-
-    return { redisInfo, runtimeInfo }
+        return {
+          ...acc,
+          [key]: val
+        }
+      }, {})
+      return { redisInfo, runtimeInfo }
+    } catch (err) {
+      return { redisInfo: {}, runtimeInfo }
+    }
   }
 }
 
