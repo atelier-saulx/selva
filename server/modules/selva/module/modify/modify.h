232fc975bff12ed5e5374776018f8d553ca567f1
Tony Kovanen <tony.kovanen@gmail.com>
2020-05-26 12:25:38 +0200

    yesh

--- server/modules/selva/module/modify/modify.h
+++ server/modules/selva/module/modify/modify.h
@@ -4,15 +4,38 @@
 
 enum SelvaModify_ArgType {
   SELVA_MODIFY_ARG_VALUE = '0',
-  SELVA_MODIFY_ARG_INDEXED_VALUE = '1'
+  SELVA_MODIFY_ARG_INDEXED_VALUE = '1',
+  SELVA_MODIFY_ARG_DEFAULT = '2',
+  SELVA_MODIFY_ARG_DEFAULT_INDEXED = '3',
+  SELVA_MODIFY_ARG_OP_INCREMENT = '4',
+  SELVA_MODIFY_ARG_OP_REFERENCES = '5'
 };
 
-// struct SelvaModify_Basic {
-//   char *$default;
-//   char *$value;
-//   char *$ref;
-//   char *$increment;
-// };
+struct SelvaModify_OpIncrement {
+  boolean indexed;
+
+  char *$default;
+  size_t $default_len;
+
+  char *$increment;
+  size_t $increment_len;
+};
+
+
+struct SelvaModify_OpReferences {
+  // filled with multiple ids of length 10
+  char *$add;
+  size_t $add_len;
+
+  // filled with multiple ids of length 10
+  char *$delete;
+  size_t $delete_len;
+
+
+  // filled with multiple ids of length 10
+  char *$value;
+  size_t $value_len;
+};
 
 enum SelvaModify_AsyncTaskType {
   SELVA_MODIFY_ASYNC_TASK_PUBLISH = 0,
@@ -21,9 +44,12 @@ enum SelvaModify_AsyncTaskType {
 
 struct SelvaModify_AsyncTask {
   enum SelvaModify_AsyncTaskType type;
+
   char id[10];
+
   const char *field_name;
   size_t field_name_len;
+
   const char *value;
   size_t value_len;
 };
