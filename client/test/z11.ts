6ac36b2c7c99c76e4c1ba51fe2a156b3b00ce45c
youzi <youri@saulx.com>
2020-06-01 12:59:02 +0200

    test

---
+++ client/test/z11.ts
@@ -0,0 +1,45 @@
+import test from 'ava'
+import { connect } from '../src/index'
+import { start } from '@saulx/selva-server'
+import './assertions'
+import { wait } from './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  const client = connect({ port })
+  await client.updateSchema({
+    rootType: {
+      fields: {
+        menu: { type: 'references' }
+      }
+    }
+  })
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  const d = Date.now()
+  await client.delete('root')
+  console.log('removed', Date.now() - d, 'ms')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('inherit references $list', async t => {
+  const client = connect({ port }, { loglevel: 'info' })
+  try {
+    const res = await client.get({
+      pollId: 'RandomPollName'
+    })
+    t.pass()
+  } catch (e) {
+    console.error(e)
+    t.fail()
+  }
+})
