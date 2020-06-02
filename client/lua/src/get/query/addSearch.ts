16d6ea31e938e61af0147f85822b5cd21b763159
Jim de Beer <beerdejim@gmail.com>
2020-06-02 13:57:58 +0200

    fix

--- client/lua/src/get/query/addSearch.ts
+++ client/lua/src/get/query/addSearch.ts
@@ -22,7 +22,8 @@ function addSearch(filter: Filter): [string[], null | string] {
       ]
     }
   } else {
-    return [[], `Cannot search fields that are not indexed ${filter.$field}`]
+    return [[], null]
+    // return [[], `Cannot search fields that are not indexed ${filter.$field}`]
   }
 
   return [search, null]
