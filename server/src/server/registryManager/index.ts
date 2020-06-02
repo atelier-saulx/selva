e959c594aa22c330cb97442ab0674340325c32c9
Jim de Beer <beerdejim@gmail.com>
2020-06-02 11:03:17 +0200

    registry manager start

--- server/src/server/registryManager/index.ts
+++ server/src/server/registryManager/index.ts
@@ -1,5 +1,15 @@
 import { SelvaServer } from '../'
 
 export const registryManager = (server: SelvaServer): Promise<void> => {
-  return new Promise(resolve => {})
+  return new Promise(resolve => {
+    // listener on registry update info
+
+    // if info < 30 seconds unregister server
+
+    console.log('start registry manager')
+  })
 }
+
+// then fix the client destroy methods
+
+// add marker from schema subs
