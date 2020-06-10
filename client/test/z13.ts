4870ef5a4786265a8c6d94d7ef941085046f9d55
Tony Kovanen <tony.kovanen@gmail.com>
2020-06-10 12:25:25 +0200

    inherit fixing

--- client/test/z13.ts
+++ client/test/z13.ts
@@ -54,7 +54,7 @@ test.before(async t => {
 })
 
 test.after(async _t => {
-  const client = connect({ port })
+  const client = connect({ port }, { loglevel: 'info' })
   const d = Date.now()
   await client.delete('root')
   console.log('removed', Date.now() - d, 'ms')
