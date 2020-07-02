ec63d56ffbc949f04a97d2fb6e27c98bb681852e
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-07-02 15:50:47 +0200

    Cleanup id.c

--- server/modules/selva/id/id.c
+++ server/modules/selva/id/id.c
@@ -3,26 +3,32 @@
 
 #include "./id.h"
 
-char *genUuid(char *uuid_str) {
+static char *genUuid(char *uuid_str) {
   uuid_t uuid;
+
   uuid_generate(uuid);
   uuid_unparse_lower(uuid, uuid_str);
+
   return uuid_str;
 }
 
-int hash(const char *prefix, char *hash_str, char *str, size_t strlen) {
+static int hash(const char *prefix, char *hash_str, char *str, size_t slen) {
   int hash = 5381;
-  size_t i = strlen;
+  size_t i = slen;
+
   while (i) {
     hash = (hash * 33) ^ (int)(str[--i]);
   }
+
   return sprintf(hash_str, "%s%08x", prefix, (unsigned int)hash >> 0);
 }
 
 
 int SelvaId_GenId(const char *prefix, char *hash_str) {
   char uuid_str[37];
+
   genUuid(uuid_str);
-  hash(prefix, hash_str, uuid_str, 37);
+  hash(prefix, hash_str, uuid_str, sizeof(uuid_str));
+
   return 0;
 }
