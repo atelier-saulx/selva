0fbfacec708e9f694ff26c94e266b7b32f43ed6d
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-07-30 16:07:46 +0200

    Add delete_all to the setRecordDef record type

--- client/src/set/modifyDataRecords.ts
+++ client/src/set/modifyDataRecords.ts
@@ -2,6 +2,7 @@ import { compile } from 'data-record'
 
 export const setRecordDef = compile([
   { name: 'is_reference', type: 'int8' },
+  { name: 'delete_all', type: 'int8' },
   { name: '$add', type: 'cstring_p' },
   { name: '$delete', type: 'cstring_p' },
   { name: '$value', type: 'cstring_p' },
