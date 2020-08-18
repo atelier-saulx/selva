858f74b0e6bb63c69a7a57525e2e2eff2bb7d3b1
youzi <youri@saulx.com>
2020-08-18 12:08:29 +0200

    added fix for missing counter in sendUpdate

--- server/src/server/subscriptionManager/update/sendUpdate.ts
+++ server/src/server/subscriptionManager/update/sendUpdate.ts
@@ -23,99 +23,107 @@ const sendUpdate = async (
   const getOptions = subscription.get
   getOptions.$includeMeta = true
 
-  if (channel.startsWith(constants.SCHEMA_SUBSCRIPTION)) {
-    const dbName = channel.slice(constants.SCHEMA_SUBSCRIPTION.length + 1)
-    const schemaResp = await client.getSchema(dbName)
-    const version = schemaResp.schema.sha
-    const value = JSON.stringify({ type: 'update', payload: schemaResp.schema })
-    await redis.hmset(
-      selector,
-      CACHE,
-      channel,
-      value,
-      channel + '_version',
-      version
-    )
-    await redis.publish(selector, channel, version)
-    subscription.beingProcessed = false
-    return
-  }
-
-  let time = setTimeout(() => {
-    // log these somewhere!
-    console.log('TIMEOUT OUT', channel, subscription.origins)
-  }, 15e3)
-
-  const payload = await client.get(getOptions)
-
-  // call $meta tree
-  const newTree = payload.$meta
-
-  delete payload.$meta
-
-  // make this without payload
-  const resultStr = JSON.stringify({ type: 'update', payload })
-  const currentVersion = subscription.version
-  const newVersion = hash(resultStr)
-
-  const treeVersion = subscription.treeVersion
-  const q = []
-
-  // if sub is removed
-  if (
-    subscriptionManager.subscriptions[subscription.channel] !== subscription
-  ) {
-    clearTimeout(time)
-    subscriptionManager.inProgressCount--
-    subscription.beingProcessed = false
-    return
-  }
-
-  if (newTree) {
-    const newTreeJson = JSON.stringify(newTree)
-    const newTreeVersion = hash(newTreeJson)
-    if (treeVersion !== newTreeVersion) {
-      if (treeVersion) {
-        removeSubscriptionFromTree(subscriptionManager, subscription)
-      }
-      subscription.treeVersion = newTreeVersion
-      subscription.tree = newTree
-      addSubscriptionToTree(subscriptionManager, subscription)
-      q.push(redis.hset(selector, CACHE, channel + '_tree', newTreeJson))
-    }
-  } else if (treeVersion) {
-    // remove tree ?
-  }
-
-  if (currentVersion === newVersion) {
-    clearTimeout(time)
-    subscriptionManager.inProgressCount--
-    if (subscription.processNext) {
-      await wait(100)
-      subscription.processNext = false
-      await sendUpdate(subscriptionManager, subscription)
-    } else {
+  try {
+    if (channel.startsWith(constants.SCHEMA_SUBSCRIPTION)) {
+      const dbName = channel.slice(constants.SCHEMA_SUBSCRIPTION.length + 1)
+      const schemaResp = await client.getSchema(dbName)
+      const version = schemaResp.schema.sha
+      const value = JSON.stringify({
+        type: 'update',
+        payload: schemaResp.schema
+      })
+      await redis.hmset(
+        selector,
+        CACHE,
+        channel,
+        value,
+        channel + '_version',
+        version
+      )
+      await redis.publish(selector, channel, version)
+      subscriptionManager.inProgressCount--
       subscription.beingProcessed = false
+      return
     }
-    return
-  }
 
-  subscription.version = newVersion
+    var time = setTimeout(() => {
+      // log these somewhere!
+      console.log('TIMEOUT OUT', channel, subscription.origins)
+    }, 15e3)
 
-  q.push(
-    redis.hmset(
-      selector,
-      CACHE,
-      channel,
-      resultStr,
-      channel + '_version',
-      newVersion
+    const payload = await client.get(getOptions)
+
+    // call $meta tree
+    const newTree = payload.$meta
+
+    delete payload.$meta
+
+    // make this without payload
+    const resultStr = JSON.stringify({ type: 'update', payload })
+    const currentVersion = subscription.version
+    const newVersion = hash(resultStr)
+
+    const treeVersion = subscription.treeVersion
+    const q = []
+
+    // if sub is removed
+    if (
+      subscriptionManager.subscriptions[subscription.channel] !== subscription
+    ) {
+      clearTimeout(time)
+      subscriptionManager.inProgressCount--
+      subscription.beingProcessed = false
+      return
+    }
+
+    if (newTree) {
+      const newTreeJson = JSON.stringify(newTree)
+      const newTreeVersion = hash(newTreeJson)
+      if (treeVersion !== newTreeVersion) {
+        if (treeVersion) {
+          removeSubscriptionFromTree(subscriptionManager, subscription)
+        }
+        subscription.treeVersion = newTreeVersion
+        subscription.tree = newTree
+        addSubscriptionToTree(subscriptionManager, subscription)
+        q.push(redis.hset(selector, CACHE, channel + '_tree', newTreeJson))
+      }
+    } else if (treeVersion) {
+      // remove tree ?
+    }
+
+    if (currentVersion === newVersion) {
+      clearTimeout(time)
+      subscriptionManager.inProgressCount--
+      if (subscription.processNext) {
+        await wait(100)
+        subscription.processNext = false
+        await sendUpdate(subscriptionManager, subscription)
+      } else {
+        subscription.beingProcessed = false
+      }
+      return
+    }
+
+    subscription.version = newVersion
+
+    q.push(
+      redis.hmset(
+        selector,
+        CACHE,
+        channel,
+        resultStr,
+        channel + '_version',
+        newVersion
+      )
     )
-  )
 
-  await Promise.all(q)
+    await Promise.all(q)
 
-  await redis.publish(selector, channel, newVersion)
+    await redis.publish(selector, channel, newVersion)
+  } catch (e) {
+    console.error('Error in sendUpdate', e)
+  }
 
   clearTimeout(time)
 
