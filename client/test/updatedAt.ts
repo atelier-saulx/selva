42f29d3effa85ebb372eb2e8dc493dfbbd485cfc
youzi <youri@saulx.com>
2020-08-20 11:47:37 +0200

    add test for updatedAt

---
+++ client/test/updatedAt.ts
@@ -0,0 +1,129 @@
+import test from 'ava'
+import { connect } from '../src/index'
+import { start } from '@saulx/selva-server'
+import './assertions'
+import { wait } from './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+test.before(async t => {
+  port = await getPort()
+  srv = await start({
+    port
+  })
+  const client = connect({ port })
+  await client.updateSchema({
+    languages: ['en', 'de'],
+    types: {
+      match: {
+        prefix: 'ma',
+        fields: {
+          title: {
+            type: 'text',
+            search: { type: ['TEXT-LANGUAGE-SUG'] }
+          },
+          published: { type: 'boolean', search: { type: ['TAG'] } },
+          awayTeam: { type: 'reference' },
+          homeTeam: { type: 'reference' },
+          image: {
+            type: 'object',
+            properties: {
+              thumb: {
+                type: 'url'
+              },
+              cover: {
+                type: 'url'
+              }
+            }
+          },
+          video: {
+            type: 'record',
+            values: {
+              type: 'object',
+              properties: {
+                hls: {
+                  type: 'url'
+                },
+                mp4: {
+                  type: 'url'
+                }
+              }
+            }
+          }
+        }
+      },
+      team: {
+        prefix: 'te'
+      },
+      competition: {
+        prefix: 'co'
+      }
+    }
+  })
+})
+
+test.after(async _t => {
+  const client = connect({ port })
+  const d = Date.now()
+  await client.delete('root')
+  console.log('removed', Date.now() - d, 'ms')
+  await client.destroy()
+  await srv.destroy()
+})
+
+test.serial('updatedAt only changes when actually changed', async t => {
+  const client = connect({ port }, { loglevel: 'info' })
+
+  const team1 = await client.set({
+    $alias: 'myteam1',
+    name: 'team1'
+  })
+
+  const team2 = await client.set({
+    $alias: 'myteam2',
+    name: 'team2'
+  })
+
+  const competition = await client.set({
+    $alias: 'mycompetition',
+    name: 'competition'
+  })
+
+  let n = 2
+  let lastUpdatedAt
+  while (n--) {
+    await client.set({
+      $alias: 'snurkle',
+      $language: 'en',
+      published: true,
+      title: 'success',
+      homeTeam: team1,
+      awayTeam: team2,
+      image: {
+        thumb: 'https://example.com',
+        cover: 'https://example.com'
+      },
+      video: {
+        default: {
+          hls: 'https://example.com',
+          mp4: 'https://example.com'
+        }
+      },
+      parents: {
+        $add: [team1, team2, competition]
+      }
+    })
+    const { updatedAt } = await client.get({
+      $alias: 'snurkle',
+      updatedAt: true
+    })
+    if (lastUpdatedAt) {
+      t.is(lastUpdatedAt, updatedAt)
+    }
+    lastUpdatedAt = updatedAt
+  }
+
+  await client.delete('root')
+  await client.destroy()
+})
