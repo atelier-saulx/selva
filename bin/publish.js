6c78521aba0894183f51b632c6a6ed5dad88df90
Tony Kovanen <tony.kovanen@gmail.com>
2020-08-31 18:10:18 +0200

    update tools

--- bin/publish.js
+++ bin/publish.js
@@ -8,12 +8,14 @@ const path = require('path')
 const client = path.join(__dirname, '../', 'client', 'package.json')
 const server = path.join(__dirname, '../', 'server', 'package.json')
 const api = path.join(__dirname, '../', 'api', 'package.json')
+const tools = path.join(__dirname, '../', 'tools', 'package.json')
 
 const clientPkg = JSON.parse(fs.readFileSync(client).toString())
 const serverPkg = JSON.parse(fs.readFileSync(server).toString())
 const apiPkg = JSON.parse(fs.readFileSync(api).toString())
+const toolsPkg = JSON.parse(fs.readFileSync(tools).toString())
 
-const pkgs = [clientPkg, serverPkg, apiPkg]
+const pkgs = [clientPkg, serverPkg, apiPkg, toolsPkg]
 
 pkgs.forEach(v => {
   const version = v.version.split('.')
@@ -34,9 +36,12 @@ clientPkg.devDependencies['@saulx/selva-server'] = serverPkg.version
 serverPkg.dependencies['@saulx/selva'] = clientPkg.version
 apiPkg.dependencies['@saulx/selva'] = clientPkg.version
 apiPkg.devDependencies['@saulx/selva-server'] = serverPkg.version
+toolsPkg.dependencies['@saulx/selva'] = clientPkg.version
+toolsPkg.devDependencies['@saulx/selva-server'] = serverPkg.version
 
 // dependencies: @saulx/selva
 fs.writeFileSync(client, JSON.stringify(clientPkg, void 0, 2))
 fs.writeFileSync(server, JSON.stringify(serverPkg, void 0, 2))
 fs.writeFileSync(api, JSON.stringify(apiPkg, void 0, 2))
+fs.writeFileSync(tools, JSON.stringify(toolsPkg, void 0, 2))
 console.log('update version --', bump)
