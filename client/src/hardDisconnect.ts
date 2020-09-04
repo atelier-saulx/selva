1d5ade225306d6c36ae6e5ea25623f4f2f5f660d
Jim de Beer <beerdejim@gmail.com>
2020-09-04 16:16:53 +0200

    yes

--- client/src/hardDisconnect.ts
+++ client/src/hardDisconnect.ts
@@ -2,10 +2,22 @@ import { Connection } from './connection'
 import { SelvaClient } from '.'
 
 export default (selvaClient: SelvaClient, connection: Connection) => {
+  const serverDescriptor = connection.serverDescriptor
+
   console.log(
     '🥞 Go somethign is hard dc arrived at selva client!',
     connection.serverDescriptor
   )
+
+  if (
+    selvaClient.server &&
+    selvaClient.server.port === serverDescriptor.port &&
+    selvaClient.server.host === serverDescriptor.host
+  ) {
+    console.log(
+      'OK THIS IS A HARD DC ON A CLIENT THAT CONNECTED TO A SERVER - THIS SHOULD NOT CALL GET SERVER WITHOUT A PORT AND HOST AGAIN'
+    )
+  }
 
   const state = connection.getConnectionState(selvaClient.selvaId)
 
