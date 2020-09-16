c8ed140609773d84c6ff710d9d7e2da3470d41c4
Jim de Beer <beerdejim@gmail.com>
2020-09-16 16:37:19 +0200

    start

--- client/src/observable/index.ts
+++ client/src/observable/index.ts
@@ -11,27 +11,33 @@ var observableIds = 0
 
 // unique ness is the resulting connect options of a selva client
 
+// give registry a unique id?
+
+// or based on registry fn :/
+
+// for now expect it to be connected to the same cluster?
+
 export class Observable {
-  constructor(options: ObservableOptions, uuid?: string) {
+  constructor(options: ObservableOptions, id: string = '', uuid?: string) {
     console.log('make observable for you')
-
     // on create need to connect
-
     // what about move subscription event?
     // just go trough all the subs managers
-
     // whats wrong here is that we dont know if you connect to multiple registries
     if (!uuid) {
       uuid = generateSubscriptionId(options)
     }
-
     this.uuid = uuid
-
     this.selvaId = String('o' + ++observableIds)
 
+    this.id = id
+
+    observables.set(uuid + id, this)
     // add to observables map
     // but - twist - need to add to specific port / host of a registry (annoying!)
   }
+
+  public id: string
 
   public uuid: string // hash of getoptions
 
@@ -51,6 +57,11 @@ export class Observable {
     // do it
   }
 
+  public destroy() {
+    observables.delete(this.uuid + this.id)
+    // emit
+  }
+
   public removeClient(client: SelvaClient): boolean {
     const hasClient = this.clients.has(client)
     if (hasClient) {
@@ -63,11 +74,15 @@ export class Observable {
   }
 }
 
-export const createObservable = (options: ObservableOptions) => {
+// maybe have to remove the re-use... :/
+export const createObservable = (
+  options: ObservableOptions,
+  id: string = ''
+) => {
   const uuid = generateSubscriptionId(options)
-  let observable = observables.get(uuid)
+  let observable = observables.get(uuid + id)
   if (!observable) {
-    observable = new Observable(options, uuid)
+    observable = new Observable(options, id, uuid)
   }
   return observable
 }
