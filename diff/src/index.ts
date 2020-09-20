5c036cd415dc95e01cfe870be75c1ffa96150a1c
Jim de Beer <beerdejim@gmail.com>
2020-09-20 13:35:55 +0200

    correct build folder

--- diff/src/index.ts
+++ diff/src/index.ts
@@ -1,10 +1,12 @@
 console.log('hello')
-const testAddon = require('../../build/Release/testaddon.node')
+const testAddon = require('../build/Release/testaddon.node')
 
 console.log('????', testAddon)
 
 console.log(testAddon.hello('x', 'y'))
 // include the c bindings
+
+// start with object diff!
 
 export const diff = () => {}
 
