61cfd189f5fc6db2d66c8802418bfc8145ca9ee9
Jim de Beer <beerdejim@gmail.com>
2020-10-05 13:18:29 +0200

    fix

--- query-ast-parser/test/index.ts
+++ query-ast-parser/test/index.ts
@@ -1,10 +1,8 @@
 import test from 'ava'
-import { createAst, printAst } from '../src'
+import { createAst, printAst, createRpn } from '../src'
 import { Filter, GeoFilter } from '../../client/src/get/types'
 
 test('basic filter', async t => {
-  console.log('go go go')
-
   const filter: Filter[] = [
     {
       $field: 'type',
@@ -20,9 +18,22 @@ test('basic filter', async t => {
 
   const ast = createAst(filter)
 
+  t.deepEqual(ast, {
+    isFork: true,
+    $and: [
+      { $value: 'team', $operator: '=', $field: 'type' },
+      { $value: 2, $operator: '!=', $field: 'value' }
+    ]
+  })
+
+  const rpn = createRpn(filter)
+
   printAst(ast)
 
-  console.log({ ast })
+  t.deepEqual(rpn, [
+    undefined,
+    [' $2 $1 f c @4 $3 g G M', 'type', 'team', 'value', '2']
+  ])
 
   t.pass()
 })
