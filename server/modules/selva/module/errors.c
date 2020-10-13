081541f427371249ba8e7b84d1fa3cfe937bef2b
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-10-13 09:08:42 +0200

    Warning fix

--- server/modules/selva/module/errors.c
+++ server/modules/selva/module/errors.c
@@ -32,7 +32,7 @@ const char *getSelvaErrorStr(int err) {
 }
 
 /* declared as weak so the unit tests can override it. */
-int replyWithSelvaError(RedisModuleCtx *ctx, int err) __attribute__((weak)) {
+__attribute__((weak)) int replyWithSelvaError(RedisModuleCtx *ctx, int err) {
     return RedisModule_ReplyWithError(ctx, getSelvaErrorStr(err));
 }
 
