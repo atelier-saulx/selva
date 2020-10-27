156a0968c0d5c4d851596ab498225f2d9f038e35
Tony Kovanen <tony.kovanen@gmail.com>
2020-10-27 10:10:10 +0100

    clean up delete.ts test

--- client/test/delete.ts
+++ client/test/delete.ts
@@ -6,7 +6,7 @@ import { dumpDb } from './assertions'
 import getPort from 'get-port'
 import { wait } from '../src/util'
 
-const DEFAULT_HIERARCHY = '___selva_hierarchy';
+const DEFAULT_HIERARCHY = '___selva_hierarchy'
 
 let srv
 let port: number
@@ -127,174 +127,14 @@ test.serial('can delete root', async t => {
 
   t.deepEqual(root, 'root')
   t.deepEqual(await client.redis.hget('root', 'value'), '9001')
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-
-  await client.delete('root')
-  t.deepEqual(await dumpDb(client), [ [ 'root', { '$id': 'root', type: 'root' } ] ])
-
-  await client.destroy()
-})
-
-test.serial('can delete a field', async t => {
-  const client = connect(
-    {
-      port
-    },
-    { loglevel: 'info' }
+  t.deepEqual(
+    await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'),
+    [match]
   )
 
-  const match = await client.set({
-    type: 'match',
-    value: 9002
-  })
-
-  await client.set({
-    $id: 'root',
-    value: 9001
-  })
-
-  t.deepEqual(await client.redis.hget('root', 'value'), '9001')
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget(match, 'value'), '9002')
-
-  await client.delete({
-    $id: 'root',
-    value: true
-  })
-
-  t.deepEqual(await client.redis.hexists('root', 'value'), 0)
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget(match, 'value'), '9002')
-
-  await client.delete({
-    $id: match,
-    value: true
-  })
-
-  t.deepEqual(await client.redis.hexists('root', 'value'), 0)
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hexists(match, 'value'), 0)
-
-  await client.delete({
-    $id: 'root',
-    children: true
-  })
-
-  t.deepEqual(await client.redis.hexists('root', 'value'), 0)
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [])
-  t.deepEqual(await client.redis.hexists(match, 'value'), 0)
-
   await client.delete('root')
-  await client.delete(match)
-  await client.destroy()
-})
+  t.deepEqual(await dumpDb(client), [['root', { $id: 'root', type: 'root' }]])
 
-test.serial('can delete all but some fields', async t => {
-  const client = connect(
-    {
-      port
-    },
-    { loglevel: 'info' }
-  )
-
-  const match = await client.set({
-    type: 'match',
-    value: 9002,
-    title: { en: 'yes yeesh', de: 'ja ja' }
-  })
-
-  await client.set({
-    $id: 'root',
-    value: 9001,
-    title: { en: 'no noes', de: 'nein nein' }
-  })
-
-  t.deepEqual(await client.redis.hget('root', 'value'), '9001')
-  t.deepEqual(await client.redis.hget('root', 'title.en'), 'no noes')
-  t.deepEqual(await client.redis.hget('root', 'title.de'), 'nein nein')
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget(match, 'value'), '9002')
-  t.deepEqual(await client.redis.hget(match, 'title.en'), 'yes yeesh')
-  t.deepEqual(await client.redis.hget(match, 'title.de'), 'ja ja')
-
-  await client.delete({
-    $id: 'root',
-    title: {
-      en: false
-    }
-  })
-
-  t.deepEqual(await client.redis.hexists('root', 'value'), 1)
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget('root', 'title.en'), 'no noes')
-  t.deepEqual(await client.redis.hexists('root', 'title.de'), 0)
-
-  await client.delete({
-    $id: match,
-    title: {
-      en: false
-    }
-  })
-
-  t.deepEqual(await client.redis.hexists(match, 'value'), 1)
-  t.deepEqual(await client.redis.hget(match, 'title.en'), 'yes yeesh')
-  t.deepEqual(await client.redis.hexists(match, 'title.de'), 0)
-
-  await client.delete({
-    $id: 'root',
-    title: {
-      en: true
-    },
-    children: true
-  })
-
-  t.deepEqual(await client.redis.hexists('root', 'title.en'), 0)
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [])
-
-  await client.delete('root')
-  await client.delete(match)
-  await client.destroy()
-})
-
-test.serial('can delete a field when only nested specified', async t => {
-  const client = connect(
-    {
-      port
-    },
-    { loglevel: 'info' }
-  )
-
-  const match = await client.set({
-    type: 'match',
-    title: {
-      en: 'yes text',
-      de: 'ja text'
-    }
-  })
-
-  await client.set({
-    $id: 'root',
-    value: 9001
-  })
-
-  t.deepEqual(await client.redis.hget('root', 'value'), '9001')
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget(match, 'title.de'), 'ja text')
-  t.deepEqual(await client.redis.hget(match, 'title.en'), 'yes text')
-
-  await client.delete({
-    $id: match,
-    title: {
-      en: true
-    }
-  })
-
-  t.deepEqual(await client.redis.hexists(match, 'title.en'), 0)
-  t.deepEqual(await client.redis.hget('root', 'value'), '9001')
-  t.deepEqual(await client.redis.selva_hierarchy_children(DEFAULT_HIERARCHY, 'root'), [match])
-  t.deepEqual(await client.redis.hget(match, 'title.de'), 'ja text')
-  await client.delete('root')
-  await client.delete(match)
   await client.destroy()
 })
 
