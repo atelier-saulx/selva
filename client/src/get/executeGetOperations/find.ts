1edcf9de92a0abe4caeb65822f28674814a8d672
Tony Kovanen <tony.kovanen@gmail.com>
2020-11-10 12:46:06 +0200

    progress

--- client/src/get/executeGetOperations/find.ts
+++ client/src/get/executeGetOperations/find.ts
@@ -1,8 +1,50 @@
 import { SelvaClient } from '../../'
-import { GetOperationFind, GetResult, GetOperation } from '../types'
+import { GetOperationFind, GetResult, GetOperation, GetOptions } from '../types'
 import { ast2rpn, Fork, FilterAST, isFork } from '@saulx/selva-query-ast-parser'
 import { executeNestedGetOperations, ExecContext, addMarker } from './'
 import { padId, joinIds } from '../utils'
+import { setNestedResult } from '../utils'
+
+function parseGetOpts(
+  props: GetOptions,
+  path: string
+): [Set<string>, GetOptions[]] {
+  const pathPrefix = path === '' ? '' : path + '.'
+  const fields: Set<string> = new Set()
+
+  const gets: GetOptions[] = []
+  for (const k in props) {
+    if (props[k] === true) {
+      fields.add(pathPrefix + k)
+    } else if (props[k] === false) {
+      // ignore
+    } else if (typeof props[k] === 'object') {
+      const opts = Object.keys(props[k]).filter(p => p.startsWith('$'))
+      if ((path === '' || opts.length === 1) && opts[0] === '$field') {
+        for (const f in props[k].$field) {
+          fields.add(f)
+        }
+      } else if (path !== '' && opts.length >= 1) {
+        const o = {}
+        setNestedResult(o, pathPrefix + k, props[k])
+        gets.push(o)
+      } else if (!k.startsWith('$')) {
+        const [nestedFields, nestedGets] = parseGetOpts(
+          props[k],
+          pathPrefix + k
+        )
+
+        for (const f of nestedFields.values()) {
+          fields.add(f)
+        }
+
+        gets.push(...nestedGets)
+      }
+    }
+  }
+
+  return [fields, gets]
+}
 
 function findTimebased(ast: Fork): FilterAST[] {
   if (!ast) {
@@ -181,6 +223,147 @@ const findIds = async (
   } else if (Array.isArray(op.sourceField)) {
     sourceField = op.sourceField.join('\n')
   }
+  const args = op.filter ? ast2rpn(op.filter, lang) : ['#1']
+  // TODO: change this if ctx.subId (for markers)
+  if (op.inKeys) {
+    // can make this a bit better....
+    const ids = await client.redis.selva_hierarchy_findin(
+      {
+        name: db
+      },
+      '___selva_hierarchy',
+      'order',
+      op.options.sort?.$field || '',
+      op.options.sort?.$order || 'asc',
+      'offset',
+      op.options.offset,
+      'limit',
+      op.options.limit,
+      joinIds(op.inKeys),
+      ...args
+    )
+
+    await checkForNextRefresh(
+      ctx,
+      client,
+      sourceField,
+      joinIds(op.inKeys),
+      op.filter,
+      lang
+    )
+
+    return ids
+  } else {
+    const realOpts: any = {}
+    for (const key in op.props) {
+      if (!key.startsWith('$')) {
+        realOpts[key] = true
+      }
+    }
+
+    if (op.nested) {
+      let added = false
+      for (let i = 0; i < op.id.length; i += 10) {
+        let endLen = 10
+        while (op.id[i + endLen - 1] === '\0') {
+          endLen--
+        }
+        const id = op.id.slice(i, endLen)
+
+        const r = await addMarker(client, ctx, {
+          type: sourceField,
+          id: id,
+          fields: op.props.$all === true ? [] : Object.keys(realOpts),
+          rpn: args
+        })
+
+        added = added || r
+
+        await checkForNextRefresh(ctx, client, sourceField, id, op.filter, lang)
+      }
+
+      if (added) {
+        ctx.hasFindMarkers = true
+      }
+    } else {
+      const added = await addMarker(client, ctx, {
+        type: sourceField,
+        id: op.id,
+        fields: op.props.$all === true ? [] : Object.keys(realOpts),
+        rpn: args
+      })
+
+      if (added) {
+        ctx.hasFindMarkers = true
+      }
+    }
+
+    console.log('HMM', parseGetOpts(op.props, ''))
+    const ids = await client.redis.selva_hierarchy_find(
+      {
+        name: db
+      },
+      '___selva_hierarchy',
+      'bfs',
+      sourceField,
+      'order',
+      op.options.sort?.$field || '',
+      op.options.sort?.$order || 'asc',
+      'offset',
+      op.options.offset,
+      'limit',
+      op.options.limit,
+      padId(op.id),
+      ...args
+    )
+
+    await checkForNextRefresh(
+      ctx,
+      client,
+      sourceField,
+      padId(op.id),
+      op.filter,
+      lang
+    )
+
+    return ids
+  }
+}
+
+const findFields = async (
+  client: SelvaClient,
+  op: GetOperationFind,
+  lang: string,
+  ctx: ExecContext
+): Promise<string[]> => {
+  const { db, subId } = ctx
+
+  let sourceField: string = <string>op.sourceField
+  if (typeof op.props.$list === 'object' && op.props.$list.$inherit) {
+    const res = await executeNestedGetOperations(
+      client,
+      {
+        $db: ctx.db,
+        $id: op.id,
+        result: {
+          $field: op.sourceField,
+          $inherit: op.props.$list.$inherit
+        }
+      },
+      lang,
+      ctx
+    )
+
+    op.inKeys = res.result
+  } else if (Array.isArray(op.sourceField)) {
+    sourceField = op.sourceField.join('\n')
+  }
+
+  // TODO: parse the props into 'fields' arg and "leftover gets"
+  const fields = {}
+  for (const key in op.props) {
+  }
+
   const args = op.filter ? ast2rpn(op.filter, lang) : ['#1']
   // TODO: change this if ctx.subId (for markers)
   if (op.inKeys) {
