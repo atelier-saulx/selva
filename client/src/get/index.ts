901248b889e66252b80de6ea780ccab12a9d5119
Tony Kovanen <tony.kovanen@gmail.com>
2020-11-16 11:11:19 +0200

    fix

--- client/src/get/index.ts
+++ client/src/get/index.ts
@@ -93,7 +93,7 @@ async function get(
 
   console.log('GET DB', db)
 
-  await client.initializeSchema({ db })
+  await client.initializeSchema({ $db: db })
 
   const extraQueries: ExtraQueries = {}
   await validate(extraQueries, client, props)
