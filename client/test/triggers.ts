9e36fa318de8ec9bbbeb25fa52be686a81d1c09e
Tony Kovanen <tony.kovanen@gmail.com>
2021-01-25 11:57:43 +0100

    added delete test (still needs filter)

--- client/test/triggers.ts
+++ client/test/triggers.ts
@@ -177,7 +177,12 @@ test.serial('basic trigger updated subscriptions', async t => {
     yesh: 'nice'
   })
 
-  t.plan(1)
+  const thing2 = await client.set({
+    type: 'noType',
+    no: 'initial value'
+  })
+
+  t.plan(2)
 
   let o2counter = 0
   const other = client.observeEvent('updated', {
@@ -203,6 +208,13 @@ test.serial('basic trigger updated subscriptions', async t => {
         type: 'yeshType',
         yesh: 'extra nice'
       })
+    } else if (o2counter === 1) {
+      // gets start event
+      t.deepEqualIgnoreOrder(d, {
+        id: thing2,
+        type: 'noType',
+        no: 'hmm'
+      })
     } else {
       t.fail
     }
@@ -212,8 +224,152 @@ test.serial('basic trigger updated subscriptions', async t => {
   await wait(500)
 
   await client.set({
+    type: 'yeshType',
+    yesh: 'new node, no event'
+  })
+
+  await client.set({
+    type: 'noType',
+    no: 'new node, no event'
+  })
+
+  await client.set({
     $id: thing,
     yesh: 'extra nice'
+  })
+
+  await client.set({
+    $id: thing2,
+    no: 'hmm'
+  })
+
+  try {
+    const subs = await client.redis.selva_subscriptions_list(
+      '___selva_hierarchy'
+    )
+
+    const sub = subs[0]
+    console.log(
+      await client.redis.selva_subscriptions_debug('___selva_hierarchy', sub)
+    )
+  } catch (e) {
+    console.log('wtf', e)
+  }
+
+  await wait(500 * 2)
+
+  await client.set({
+    $id: 'root',
+    no: 'no event pls'
+  })
+
+  await client.set({
+    $id: 'noNoNoNo',
+    no: 'no event again'
+  })
+
+  // no event
+  await client.delete({
+    $id: thing
+  })
+
+  await wait(500 * 2)
+
+  sub2.unsubscribe()
+
+  await wait(500 * 2)
+
+  await client.delete('root')
+
+  await wait(1000)
+
+  await client.destroy()
+})
+
+test.serial('basic trigger deleted subscriptions', async t => {
+  const client = connect({ port })
+
+  await client.updateSchema({
+    languages: ['en', 'de', 'nl'],
+    rootType: {
+      fields: { yesh: { type: 'string' }, no: { type: 'string' } }
+    },
+    types: {
+      yeshType: {
+        prefix: 'ye',
+        fields: {
+          yesh: { type: 'string' }
+        }
+      },
+      noType: {
+        prefix: 'no',
+        fields: {
+          no: { type: 'string' }
+        }
+      }
+    }
+  })
+
+  await client.set({ $id: 'root' })
+  const thing = await client.set({
+    type: 'yeshType',
+    yesh: 'nice'
+  })
+
+  const thing2 = await client.set({
+    type: 'noType',
+    no: 'initial value'
+  })
+
+  t.plan(1)
+
+  let o2counter = 0
+  const other = client.observeEvent('deleted', {
+    // $filter: {
+    //   $operator: '=',
+    //   $field: 'type',
+    //   $value: 'yeshType',
+    //   $or: {
+    //     $operator: '=',
+    //     $field: 'type',
+    //     $value: 'noType'
+    //   }
+    // },
+    $all: true,
+    aliases: false
+  })
+
+  const sub2 = other.subscribe(d => {
+    if (o2counter === 0) {
+      // yes
+      console.log('d', d)
+      t.pass()
+    } else {
+      t.fail
+    }
+    o2counter++
+  })
+
+  await wait(500)
+
+  await client.set({
+    type: 'yeshType',
+    yesh: 'new node, no event'
+  })
+
+  await client.set({
+    type: 'noType',
+    no: 'new node, no event'
+  })
+
+  await client.set({
+    $id: thing,
+    yesh: 'extra nice'
+  })
+
+  await client.set({
+    $id: thing2,
+    no: 'hmm'
   })
 
   try {
