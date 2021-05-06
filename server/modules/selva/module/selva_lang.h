59416190f340b3c5deb217be663090ef0318a331
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-05-06 15:35:17 +0200

    SelvaLang_Strcasexfrm() was removed

--- server/modules/selva/module/selva_lang.h
+++ server/modules/selva/module/selva_lang.h
@@ -4,12 +4,4 @@
 
 locale_t SelvaLang_GetLocale(const char *lang_str, size_t lang_len);
 
-/**
- * Transforms a given string into a locale dependant collated
- * case-insensitive non-human-readable binary representation that works works
- * with strcmp().
- * If dst is NULL then a number of bytes needed for the destination is returned.
- */
-size_t SelvaLang_Strcasexfrm(char *dst, const char *src, locale_t locale);
-
 #endif /* SELVA_LANG */
