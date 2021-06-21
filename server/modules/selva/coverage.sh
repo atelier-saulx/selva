bdd43a5addd96974296275b1632618051151130b
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-06-21 14:36:36 +0200

    Generate coverage reports properly on mac

--- server/modules/selva/coverage.sh
+++ server/modules/selva/coverage.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env sh
-cd test/units
-gcov "../../$1" -m -k -j -q -t --object-directory obj
+#!/usr/bin/env bash
+#gcov "../../$1" -m -k -j -q -t --object-directory obj
+mkdir -p gcov/reports
+cd gcov/reports
+for o in `ls ../../test/units/obj |grep '\.o' |sed 's/\.o$//'`; do
+    gcov "$o" --object-directory ../../test/units/obj
+done
