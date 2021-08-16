438e620cf74df1f800cf0d138de0a4fb71d2a677
Nuno Frade <nfrade.tribal@gmail.com>
2021-08-16 23:41:11 +0200

    Adds failing array replace test

---
+++ client/test/z_array_replace.ts
@@ -0,0 +1,121 @@
+import test from 'ava'
+import { connect } from '../src/index'
+import { start } from '@saulx/selva-server'
+import './assertions'
+import getPort from 'get-port'
+
+let srv
+let port: number
+
+test.before(async (t) => {
+  port = await getPort()
+  srv = await start({
+    port,
+  })
+  await new Promise((resolve, _reject) => {
+    setTimeout(resolve, 100)
+  })
+})
+
+test.beforeEach(async (t) => {
+  const client = connect({ port }, { loglevel: 'info' })
+
+  await client.redis.flushall()
+  await client.updateSchema({
+    languages: ['en'],
+    types: {
+      lekkerType: {
+        prefix: 'vi',
+        fields: {
+          media: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                src: { type: 'url' },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+
+  // A small delay is needed after setting the schema
+  await new Promise((r) => setTimeout(r, 100))
+
+  await client.destroy()
+})
+
+test.after(async (t) => {
+  const client = connect({ port })
+  await client.delete('root')
+  await client.destroy()
+  await srv.destroy()
+  await t.connectionsAreEmpty()
+})
+
+test.serial('should replace array', async (t) => {
+  const client = connect({ port }, { loglevel: 'info' })
+
+  const originalMedia = [
+    { src: 'http://wawa.com/111' },
+    { src: 'http://wawa.com/222' },
+    { src: 'http://wawa.com/333' },
+  ]
+
+  const lekker = await client.set({
+    type: 'lekkerType',
+    media: originalMedia,
+  })
+  t.deepEqualIgnoreOrder(
+    await client.get({
+      $language: 'en',
+      $id: lekker,
+      id: true,
+      media: true,
+    }),
+    {
+      id: lekker,
+      media: originalMedia,
+    }
+  )
+
+  await client.set({
+    $id: lekker,
+    media: [{ src: 'http://wawa.com/222' }],
+  })
+  t.deepEqualIgnoreOrder(
+    await client.get({
+      $language: 'en',
+      $id: lekker,
+      id: true,
+      media: true,
+    }),
+    {
+      id: lekker,
+      media: [{ src: 'http://wawa.com/222' }],
+    }
+  )
+
+  await client.set({
+    $id: lekker,
+    media: [],
+  })
+
+  t.deepEqualIgnoreOrder(
+    await client.get({
+      $language: 'en',
+      $id: lekker,
+      id: true,
+      media: true,
+    }),
+    {
+      id: lekker,
+      media: [],
+    }
+  )
+
+  await client.delete('root')
+  await client.destroy()
+})
