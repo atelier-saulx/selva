1fb4b5fa64b74b597726a7e01551d61b0d0d2bb3
Tony Kovanen <tony.kovanen@gmail.com>
2021-08-23 14:08:38 +0200

    yes

--- client/src/set/fieldParsers/array.ts
+++ client/src/set/fieldParsers/array.ts
@@ -12,6 +12,69 @@ const ITEM_TYPES: Record<string, number> = {
   record: 4,
 }
 
+function sendPush(
+  typeArg: Buffer,
+  client: SelvaClient,
+  schema: Schema,
+  field: string,
+  payload: SetOptions,
+  result: (string | Buffer)[],
+  fields: FieldSchemaArrayLike,
+  type: string,
+  $lang?: string
+) {
+  result.push('D', field, typeArg)
+
+  const fieldWithIdx = `${field}[-1]`
+  const itemsFields = fields.items
+  const parser = fieldParsers[itemsFields.type]
+  parser(
+    client,
+    schema,
+    fieldWithIdx,
+    payload,
+    result,
+    itemsFields,
+    type,
+    $lang
+  )
+}
+
+function sendInsert(
+  typeArg: Buffer,
+  idx: number,
+  client: SelvaClient,
+  schema: Schema,
+  field: string,
+  payload: SetOptions,
+  result: (string | Buffer)[],
+  fields: FieldSchemaArrayLike,
+  type: string,
+  $lang?: string
+) {
+  result.push('E', field, typeArg)
+
+  if (!Number.isInteger(idx) || !payload) {
+    throw new Error(
+      `$assign missing $idx or $value property ${JSON.stringify(payload)}`
+    )
+  }
+
+  const fieldWithIdx = `${field}[${idx}]`
+  const itemsFields = fields.items
+  const parser = fieldParsers[itemsFields.type]
+  parser(
+    client,
+    schema,
+    fieldWithIdx,
+    payload,
+    result,
+    itemsFields,
+    type,
+    $lang
+  )
+}
+
 export default async (
   client: SelvaClient,
   schema: Schema,
@@ -31,40 +94,91 @@ export default async (
       const itemType = ITEM_TYPES[fields.items.type]
       const content = new Uint32Array([itemType])
       const buf = Buffer.from(content.buffer)
-      result.push('D', field, buf)
+      if (Array.isArray(payload.$push)) {
+        for (const el of payload.$push) {
+          sendPush(buf, client, schema, field, el, result, fields, type, $lang)
+        }
+      } else {
+        sendPush(
+          buf,
+          client,
+          schema,
+          field,
+          payload.$push,
+          result,
+          fields,
+          type,
+          $lang
+        )
+      }
+      // result.push('D', field, buf)
 
-      const fieldWithIdx = `${field}[-1]`
-      const itemsFields = fields.items
-      const parser = fieldParsers[itemsFields.type]
-      parser(
-        client,
-        schema,
-        fieldWithIdx,
-        payload.$push,
-        result,
-        itemsFields,
-        type,
-        $lang
-      )
+      // const fieldWithIdx = `${field}[-1]`
+      // const itemsFields = fields.items
+      // const parser = fieldParsers[itemsFields.type]
+      // parser(
+      //   client,
+      //   schema,
+      //   fieldWithIdx,
+      //   payload.$push,
+      //   result,
+      //   itemsFields,
+      //   type,
+      //   $lang
+      // )
     } else if (payload.$unshift) {
+      // const itemType = ITEM_TYPES[fields.items.type]
+      // const content = new Uint32Array([itemType, 0])
+      // const buf = Buffer.from(content.buffer)
+      // result.push('E', field, buf)
+
+      // const fieldWithIdx = `${field}[0]`
+      // const itemsFields = fields.items
+      // const parser = fieldParsers[itemsFields.type]
+      // parser(
+      //   client,
+      //   schema,
+      //   fieldWithIdx,
+      //   payload,
+      //   result,
+      //   itemsFields,
+      //   type,
+      //   $lang
+      // )
+
       const itemType = ITEM_TYPES[fields.items.type]
       const content = new Uint32Array([itemType, 0])
       const buf = Buffer.from(content.buffer)
-      result.push('E', field, buf)
-
-      const fieldWithIdx = `${field}[0]`
-      const itemsFields = fields.items
-      const parser = fieldParsers[itemsFields.type]
-      parser(
-        client,
-        schema,
-        fieldWithIdx,
-        payload,
-        result,
-        itemsFields,
-        type,
-        $lang
-      )
+      if (Array.isArray(payload.$unshift)) {
+        for (let i = payload.$unshift.length - 1; i >= 0; i--) {
+          const v = payload[i]
+          sendInsert(
+            buf,
+            0,
+            client,
+            schema,
+            field,
+            v,
+            result,
+            fields,
+            type,
+            $lang
+          )
+        }
+      } else {
+        sendInsert(
+          buf,
+          0,
+          client,
+          schema,
+          field,
+          payload.$unshift,
+          result,
+          fields,
+          type,
+          $lang
+        )
+      }
     } else if (payload.$assign) {
       const idx = payload.$assign.$idx
       const value = payload.$assign.$value
@@ -89,33 +203,68 @@ export default async (
         $lang
       )
     } else if (payload.$insert) {
-      const idx = payload.$insert.$idx
-      const value = payload.$insert.$value
+      // const idx = payload.$insert.$idx
+      // const value = payload.$insert.$value
 
+      // const itemType = ITEM_TYPES[fields.items.type]
+      // const content = new Uint32Array([itemType, idx])
+      // const buf = Buffer.from(content.buffer)
+      // result.push('E', field, buf)
+
+      // if (!Number.isInteger(idx) || !value) {
+      //   throw new Error(
+      //     `$assign missing $idx or $value property ${JSON.stringify(payload)}`
+      //   )
+      // }
+
+      // const fieldWithIdx = `${field}[${idx}]`
+      // const itemsFields = fields.items
+      // const parser = fieldParsers[itemsFields.type]
+      // parser(
+      //   client,
+      //   schema,
+      //   fieldWithIdx,
+      //   value,
+      //   result,
+      //   itemsFields,
+      //   type,
+      //   $lang
+      // )
+
+      const idx = payload.$insert.$idx
       const itemType = ITEM_TYPES[fields.items.type]
       const content = new Uint32Array([itemType, idx])
       const buf = Buffer.from(content.buffer)
-      result.push('E', field, buf)
-
-      if (!Number.isInteger(idx) || !value) {
-        throw new Error(
-          `$assign missing $idx or $value property ${JSON.stringify(payload)}`
+      if (Array.isArray(payload.$insert.$value)) {
+        for (let i = payload.$insert.$value.length - 1; i >= 0; i--) {
+          const v = payload.$insert.$value[i]
+          sendInsert(
+            buf,
+            idx,
+            client,
+            schema,
+            field,
+            v,
+            result,
+            fields,
+            type,
+            $lang
+          )
+        }
+      } else {
+        sendInsert(
+          buf,
+          idx,
+          client,
+          schema,
+          field,
+          payload.$insert.$value,
+          result,
+          fields,
+          type,
+          $lang
         )
       }
-
-      const fieldWithIdx = `${field}[${idx}]`
-      const itemsFields = fields.items
-      const parser = fieldParsers[itemsFields.type]
-      parser(
-        client,
-        schema,
-        fieldWithIdx,
-        value,
-        result,
-        itemsFields,
-        type,
-        $lang
-      )
     } else if (payload.$remove) {
       const content = new Uint32Array([payload.$remove.$idx])
       const buf = Buffer.from(content.buffer)
