2f21195d030fa3dae53e2599a0606e1ac19373b3
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-09-01 12:19:02 +0200

    More indexing

--- client/test/findWithIndexes.ts
+++ client/test/findWithIndexes.ts
@@ -330,50 +330,33 @@ test.serial('find index integers', async (t) => {
     })
   }
 
-  for (let i = 0; i < 500; i++) {
-    const r = await client.get({
-      $id: 'root',
-      id: true,
-      items: {
-        name: true,
-        cat: true,
-        $list: {
-          $find: {
-            $traverse: 'descendants',
-            $filter: [
-              {
-                $field: 'cat',
-                $operator: '=',
-                $value: 3,
-              },
-            ],
-          },
+  const q = async () => await client.get({
+    $id: 'root',
+    id: true,
+    items: {
+      name: true,
+      cat: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: [
+            {
+              $field: 'cat',
+              $operator: '=',
+              $value: 3,
+            },
+          ],
         },
       },
-    })
+    },
+  })
+
+  for (let i = 0; i < 500; i++) {
+    await q()
   }
   await wait(1e3)
   for (let i = 0; i < 500; i++) {
-    const r = await client.get({
-      $id: 'root',
-      id: true,
-      items: {
-        name: true,
-        cat: true,
-        $list: {
-          $find: {
-            $traverse: 'descendants',
-            $filter: [
-              {
-                $field: 'cat',
-                $operator: '=',
-                $value: 3,
-              },
-            ],
-          },
-        },
-      },
-    })
+    await q()
   }
 
   t.deepEqual(
@@ -383,4 +366,86 @@ test.serial('find index integers', async (t) => {
 
   await client.delete('root')
   await client.destroy()
+})
+
+test.serial('find index exists', async (t) => {
+  const client = connect({ port: port }, { loglevel: 'info' })
+
+  for (let i = 0; i < 1000; i++) {
+    const o = {
+      type: 'league',
+      name: `League ${i}`,
+      thing: 'abc',
+      things: ['a', 'b']
+    }
+    if (i % 2) {
+      delete o.thing
+    } else {
+      delete o.things
+    }
+    await client.set(o)
+  }
+
+  const q1 = async () => client.get({
+    $id: 'root',
+    id: true,
+    items: {
+      name: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: [
+            {
+              $field: 'thing',
+              $operator: 'exists',
+            },
+          ],
+        },
+      },
+    },
   })
+  const q2 = async () => client.get({
+    $id: 'root',
+    id: true,
+    items: {
+      name: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: [
+            {
+              $field: 'things',
+              $operator: 'notExists',
+            },
+          ],
+        },
+      },
+    },
+  })
+
+  for (let i = 0; i < 500; i++) {
+    await q1()
+  }
+  for (let i = 0; i < 500; i++) {
+    await q2()
+  }
+  await wait(2e3)
+  for (let i = 0; i < 500; i++) {
+    await q1()
+  }
+  for (let i = 0; i < 500; i++) {
+    await q2()
+  }
+  await wait(2e3)
+
+  t.deepEqual(
+    await client.redis.selva_index_list('___selva_hierarchy'),
+    [
+      'root.I.InRoaW5nIiBo', [ 500, 1001, 500, 500 ],
+      'root.I.InRoaW5ncyIgaCBM', [ 500, 1001, 500, 500 ]
+    ]
+  )
+
+  await client.delete('root')
+  await client.destroy()
+})
