bc9aa867966af11289bcc4ea77fc578de26e4093
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-09-10 11:49:27 +0200

    Unit tests for selva_set

---
+++ server/modules/selva/test/units/test-selva_set.mk
@@ -0,0 +1,6 @@
+TEST_SRC += test-selva_set.c
+SRC-selva_set += ../redis-alloc.c ../errors-mock.c
+SRC-selva_set += ../../rmutil/sds.c
+SRC-selva_set += ../../module/errors.c
+SRC-selva_set += ../../module/selva_set.c
+SRC-selva_set += ../../module/selva_type.c
