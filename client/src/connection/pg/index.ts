c97a649708a59332ffb4d2a45c3bf9b98b2ba6d6
Tony Kovanen <tony.kovanen@gmail.com>
2021-09-24 10:33:53 +0200

    more sketching

--- client/src/connection/pg/index.ts
+++ client/src/connection/pg/index.ts
@@ -1,12 +1,30 @@
 import { PG } from './client'
 
+type PGGlobalOpts = { user: string; password: string }
+type PGSelector = string | { host: string; port: number }
+
 class PGConnection {
+  private opts: PGGlobalOpts
   private clients: {
     [id: string]: PG
   }
 
-  constructor() {
+  constructor(opts: PGGlobalOpts) {
+    this.opts = opts
     this.clients = {}
+  }
+
+  getClient(selector: PGSelector) {
+    if (typeof selector === 'object') {
+      const { host, port } = selector
+      selector = `${host}:${port}`
+    }
+
+    if (!this.clients[selector]) {
+      // TODO: init client
+    }
+
+    return this.clients[selector]
   }
 }
 
