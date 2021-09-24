f5cf85ab95f34fa173f721c2ac75a375e7f8d7fb
Tony Kovanen <tony.kovanen@gmail.com>
2021-09-24 10:56:53 +0200

    yes

--- client/src/connection/pg/index.ts
+++ client/src/connection/pg/index.ts
@@ -1,10 +1,26 @@
 import { PG } from './client'
+import { QueryResult } from 'pg'
 
 type PGGlobalOpts = { user: string; password: string }
 type PGSelector = string | { host: string; port: number }
 
+function selectorToId(selector: PGSelector): string {
+  if (typeof selector === 'object') {
+    const { host, port } = selector
+    return `${host}:${port}`
+  }
+
+  return selector
+}
+
+// TODO: add cleanup based on this.lastUsed
+
 class PGConnection {
   private opts: PGGlobalOpts
+  private lastUsed: {
+    [id: string]: number
+  }
+
   private clients: {
     [id: string]: PG
   }
@@ -12,22 +28,30 @@ class PGConnection {
   constructor(opts: PGGlobalOpts) {
     this.opts = opts
     this.clients = {}
+    this.lastUsed = {}
   }
 
-  getClient(selector: PGSelector) {
-    if (typeof selector === 'object') {
-      const { host, port } = selector
-      selector = `${host}:${port}`
-    }
+  private getClient(selector: PGSelector) {
+    const id = selectorToId(selector)
 
-    if (!this.clients[selector]) {
-      const connectionString = `postgres://${this.opts.user}:${this.opts.password}@${selector}`
-      this.clients[selector] = new PG({
+    if (!this.clients[id]) {
+      const connectionString = `postgres://${this.opts.user}:${this.opts.password}@${id}`
+      this.clients[id] = new PG({
         connectionString,
       })
     }
 
-    return this.clients[selector]
+    this.lastUsed[id] = Date.now()
+    return this.clients[id]
+  }
+
+  public async execute<T>(
+    selector: PGSelector,
+    query: string,
+    params: unknown[]
+  ): Promise<QueryResult<T>> {
+    const client = this.getClient(selector)
+    return client.execute(query, params)
   }
 }
 
