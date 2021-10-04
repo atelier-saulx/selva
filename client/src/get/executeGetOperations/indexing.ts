dfb690481d8361a85a74ef252e8f0d864394fa3d
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-10-04 12:50:47 +0200

    Provide multiple indexing hints

---
+++ client/src/get/executeGetOperations/indexing.ts
@@ -0,0 +1,72 @@
+import { Schema } from '../../'
+import { GetOperationFind } from '../types'
+import {
+  Fork,
+  FilterAST,
+  isFork,
+} from '@saulx/selva-query-ast-parser'
+
+function ast2inlineRpn(schema: Schema, f: FilterAST | null): string | null {
+  if (!f) {
+    return null
+  }
+
+  if (f.$field === 'type') {
+    const prefix =
+      f.$value === 'root' ? 'ro' : schema?.types[<string>f.$value]?.prefix
+    if (!prefix) {
+      return null
+    }
+
+    if (f.$operator == '=') {
+      return `"${prefix}" e`
+    } else if (f.$operator == '!=') {
+      return `"${prefix}" e L`
+    }
+    return null
+  }
+
+  switch (f.$operator) {
+    case '=':
+      if (typeof f.$value === 'string') {
+        return `"${f.$field}" f "${f.$value}" c`
+      } else {
+        // numeric
+        return `"${f.$field}" g #${f.$value} F`
+      }
+    case 'has':
+      if (typeof f.$value === 'string') {
+        return `"${f.$value}" "${f.$field}" a`
+      } else {
+        // numeric
+        return `#${f.$value} "${f.$field}" a`
+      }
+    case 'exists':
+      return `"${f.$field}" h`
+    case 'notExists':
+      return `"${f.$field}" h L`
+  }
+
+  return null
+}
+
+export function mkIndex(schema: Schema, op: GetOperationFind): string[] {
+  if (op.id && op.id !== 'root') {
+    return []
+  }
+
+  if (!op.filter || !op.filter.$and) {
+    return []
+  }
+
+  if (op.sourceField !== 'descendants') {
+    return []
+  }
+
+  return op.filter.$and
+    .filter((f: Fork | FilterAST) => !isFork(f))
+    .map((f: FilterAST) => ast2inlineRpn(schema, f))
+    .filter((s: string | null) => s)
+    .map((s: string): string[] => ['index', s])
+    .reduce((prev, cur) => (prev.push(...cur), prev), [])
+}
