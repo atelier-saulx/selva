b0c5fae1f8c0ef61a0a545e564003fb413b99626
Tony Kovanen <tony.kovanen@gmail.com>
2021-10-25 14:30:04 +0200

    yes

--- client/src/getServer.ts
+++ client/src/getServer.ts
@@ -29,7 +29,21 @@ const getServer = (
         type = 'replica'
       }
 
-      if (type === 'subscriptionRegistry') {
+      if (type === 'timeseriesRegistry') {
+        // just get first for now - never had more then one yet...
+        // eslint-disable-next-line
+        for (let k in selvaClient.servers.tsRegisters) {
+          server = selvaClient.servers.tsRegisters[k]
+          break
+        }
+      } else if (type === 'timeseriesQueue') {
+        // just get first for now - never had more then one yet...
+        // eslint-disable-next-line
+        for (let k in selvaClient.servers.timeseriesQueues) {
+          server = selvaClient.servers.timeseriesQueues[k]
+          break
+        }
+      } else if (type === 'subscriptionRegistry') {
         // just get first for now
         // eslint-disable-next-line
         for (const k in selvaClient.servers.subRegisters) {
@@ -57,12 +71,11 @@ const getServer = (
               getServer(selvaClient, cb, selector)
             }, 2e3)
             if (!getSubInprogress[selectionOptions.subscription]) {
-              getSubInprogress[
-                selectionOptions.subscription
-              ] = selvaClient.redis.get(
-                { type: 'subscriptionRegistry' },
-                selectionOptions.subscription
-              )
+              getSubInprogress[selectionOptions.subscription] =
+                selvaClient.redis.get(
+                  { type: 'subscriptionRegistry' },
+                  selectionOptions.subscription
+                )
             }
             getSubInprogress[selectionOptions.subscription].then((serverId) => {
               if (!isCanceled) {
