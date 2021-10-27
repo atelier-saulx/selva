c53ee99d8872c3de3a943c7acd0114f8f0582105
Tony Kovanen <tony.kovanen@gmail.com>
2021-10-27 11:25:13 +0200

    run postrges binary on linux

--- server/src/server/postgresManager.ts
+++ server/src/server/postgresManager.ts
@@ -24,22 +24,32 @@ export default class PostgresManager extends ProcessManager {
     password: string
     name: string
   }) {
-    const command = `docker`
-    const args = [
-      `run`,
-      `--rm`,
-      `--name`,
-      name,
-      `-e`,
-      `POSTGRES_PASSWORD=${password}`,
-      `-p`,
-      `${port}:5432`,
-      `postgres:14beta2-alpine3.14`,
-    ]
-    super(command, {
-      args,
-      env: {},
-    })
+    const isLinux = process.platform === 'linux'
+
+    if (isLinux) {
+      const command = `postgres`
+      super(command, {
+        args: [],
+        env: {},
+      })
+    } else {
+      const command = `docker`
+      const args = [
+        `run`,
+        `--rm`,
+        `--name`,
+        name,
+        `-e`,
+        `POSTGRES_PASSWORD=${password}`,
+        `-p`,
+        `${port}:5432`,
+        `postgres:14beta2-alpine3.14`,
+      ]
+      super(command, {
+        args,
+        env: {},
+      })
+    }
 
     this.pgPort = port
     this.name = name
