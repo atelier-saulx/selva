e9676b9e17517ef95d21d5d92a7af0dbeba4593b
Jim de Beer <beerdejim@gmail.com>
2021-11-03 17:02:17 +0100

    fix

--- client/src/connection/index.ts
+++ client/src/connection/index.ts
@@ -586,7 +586,7 @@ class Connection {
 
       delete this.publisher
       delete this.subscriber
-    }, 0)
+    }, 100)
 
     this.subscriber.removeAllListeners()
     this.publisher.removeAllListeners()
