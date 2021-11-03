81abb78584f38a09bfc8c2a05289fca97142aa1c
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-11-03 15:59:59 +0100

    Share common SelvaObject key strings
    `"type"` field can only a have a few distinct value so we could
    easily share the memory location for those values.
    
    However, we can only do th sharing in very specific places where
    we have explicit control over the value string, e.g. modify and
    RDB loading.
    
    This will save about 1.5 MB of memory per 100k nodes in a db
    having just a few distinct types.
    
    Note that there is no way to free the shared strings once allocated
    but that shouldn't be a huge issue because the type strings are
    short and rarely change or go away.

---
+++ server/modules/selva/module/shared.c
@@ -0,0 +1,42 @@
+#include "selva_set.h"
+#include "redismodule.h"
+#include "cdefs.h"
+#include "shared.h"
+
+#define SELVA_SHARED_KEY_STR "type"
+#define SELVA_SHARED_KEY_LEN 4
+
+static struct SelvaSet shared_strings;
+
+RedisModuleString *Share_RMS(const char *key_str, size_t key_len, RedisModuleString *rms) {
+    RedisModuleString *out;
+
+    if (key_len != SELVA_SHARED_KEY_LEN ||
+        memcmp(key_str, SELVA_SHARED_KEY_STR, SELVA_SHARED_KEY_LEN)) {
+        return NULL;
+    }
+
+    out = SelvaSet_FindRms(&shared_strings, rms);
+    if (!out) {
+        int err;
+
+        out = RedisModule_HoldString(NULL, rms);
+        err = SelvaSet_Add(&shared_strings, out);
+        if (err) {
+            RedisModule_FreeString(NULL, rms);
+            return NULL;
+        }
+    }
+
+    /*
+     * Hodl it even if we just added it to the data structure so we can keep
+     * it in the set even if/when the caller tries to free the string.
+     */
+    RedisModule_RetainString(NULL, out);
+
+    return out;
+}
+
+__constructor static void init_shared(void) {
+    SelvaSet_Init(&shared_strings, SELVA_SET_TYPE_RMSTRING);
+}
