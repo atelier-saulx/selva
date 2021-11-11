47048041fd1bdc822264a0e43d89d4e0ae5ea024
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-11-11 14:58:56 +0100

    Implement deflate compressor for RedisModuleString
    Use libdeflate for compression and implement a wrapper for
    making transparent compression and decompression of
    RedisModuleStrings easier.

---
+++ server/modules/selva/module/rms_compressor.c
@@ -0,0 +1,85 @@
+#include <stddef.h>
+#include "libdeflate.h"
+#include "redismodule.h"
+#include "cdefs.h"
+#include "auto_free.h"
+#include "errors.h"
+#include "rms_compressor.h"
+
+int rms_compress(struct compressed_rms *out, RedisModuleString *in) {
+    struct libdeflate_compressor *compressor;
+    char *compressed_str __auto_free = NULL;
+    size_t compressed_size = 0;
+
+    TO_STR(in);
+
+    compressor = libdeflate_alloc_compressor(6);
+    if (!compressor) {
+        return SELVA_ENOMEM;
+    }
+
+    compressed_str = RedisModule_Alloc(in_len);
+    if (compressed_str) {
+        /*
+         * If mem allocation should fail we can just return the string we
+         * already have and we can just skip the compression phase.
+         */
+        compressed_size = libdeflate_deflate_compress(compressor, in_str, in_len, compressed_str, in_len);
+        libdeflate_free_compressor(compressor);
+    }
+
+    if (compressed_size == 0) {
+        /* No compression was achieved. */
+        out->uncompressed_size = -1;
+        out->rms = RedisModule_HoldString(NULL, in);
+    } else {
+        fprintf(stderr, "%s:%d: Compression ratio: %.2f:1\n",
+                __FILE__, __LINE__,
+                (double)in_len / (double)compressed_size);
+
+        out->uncompressed_size = in_len;
+        out->rms = RedisModule_CreateString(NULL, compressed_str, compressed_size);
+    }
+
+    return 0;
+}
+
+int rms_decompress(RedisModuleString **out, struct compressed_rms *in) {
+    size_t compressed_len;
+    const char *compressed_str;
+    char *uncompressed_str __auto_free = NULL;
+    struct libdeflate_decompressor *decompressor;
+    size_t nbytes_out = 0;
+    enum libdeflate_result res;
+    RedisModuleString *raw;
+
+    if (in->uncompressed_size < 0) {
+        *out = RedisModule_HoldString(NULL, in->rms);
+        return (*out) ? 0 : SELVA_ENOMEM;
+    }
+
+    uncompressed_str = RedisModule_Alloc(in->uncompressed_size);
+    if (!uncompressed_str) {
+        return SELVA_ENOMEM;
+    }
+
+    decompressor = libdeflate_alloc_decompressor();
+    if (!decompressor) {
+        return SELVA_ENOMEM;
+    }
+
+    compressed_str = RedisModule_StringPtrLen(in->rms, &compressed_len);
+    res = libdeflate_deflate_decompress(decompressor, compressed_str, compressed_len, uncompressed_str, in->uncompressed_size, &nbytes_out);
+    libdeflate_free_decompressor(decompressor);
+    if (res != 0 || nbytes_out != (size_t)in->uncompressed_size) {
+        return SELVA_EINVAL;
+    }
+
+    raw = RedisModule_CreateString(NULL, uncompressed_str, in->uncompressed_size);
+    if (!raw) {
+        return SELVA_ENOMEM;
+    }
+
+    *out = raw;
+    return 0;
+}
---
+++ server/modules/selva/module/rms_compressor.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _RMS_COMPRESSOR_H_
+#define _RMS_COMPRESSOR_H_
+
+struct compressed_rms {
+    ssize_t uncompressed_size; /*!< if equal or greater than zero then rms is compressed; Otherwise rms is uncompressed. */
+    RedisModuleString *rms;
+};
+
+static inline struct compressed_rms *rms_alloc_compressed(void) {
+    return RedisModule_Calloc(1, sizeof(struct compressed_rms));
+}
+
+static inline void rms_free_compressed(struct compressed_rms *compressed_rms) {
+    if (compressed_rms->rms) {
+        RedisModule_FreeString(NULL, compressed_rms->rms);
+    }
+    RedisModule_Free(compressed_rms);
+}
+
+int rms_compress(struct compressed_rms *out, RedisModuleString *in);
+int rms_decompress(RedisModuleString **out, struct compressed_rms *in);
+
+#endif /* _RMS_COMPRESSOR_H_ */
