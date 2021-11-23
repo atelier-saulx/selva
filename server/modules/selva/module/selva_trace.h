b4f69afd8bfc1060fe351c310c05aeb33414c66d
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-11-23 16:09:17 +0100

    Document the tracing API

--- server/modules/selva/module/selva_trace.h
+++ server/modules/selva/module/selva_trace.h
@@ -17,18 +17,34 @@ struct SelvaTrace {
 
 #ifdef SELVA_TRACE
 
+/**
+ * Create a new tracing handle.
+ * This is needed for every trace, SELVA_TRACE_BEGIN and SELVA_TRACE_END paid.
+ */
 #define SELVA_TRACE_HANDLE(_name) \
    struct SelvaTrace CONCATENATE(selva_trace_handle_, _name) = { \
        .name = #_name, \
    }; \
    DATA_SET(selva_trace, CONCATENATE(selva_trace_handle_, _name))
 
+/**
+ * Begin a new trace.
+ * Traces can be nested but different traces will appear flat in the analysis
+ * results. Bear in mind that traces are stacked so an outter trace cannot be
+ * terminated before its inner trace.
+ */
 #define SELVA_TRACE_BEGIN(name) \
     __itt_task_begin(selva_trace_domain, __itt_null, __itt_null, CONCATENATE(selva_trace_handle_, name).handle);
 
+/**
+ * End a trace.
+ */
 #define SELVA_TRACE_END(name) \
     __itt_task_end(selva_trace_domain)
 
+/**
+ * The Selva tracing domain.
+ */
 extern __itt_domain* selva_trace_domain;
 
 #else
