bff58b1b74e1ce40bc263a1b92ab251fb296fb3e
Jim de Beer <beerdejim@gmail.com>
2021-12-14 11:24:45 +0100

    fix

--- server/src/backups.ts
+++ server/src/backups.ts
@@ -1,21 +1,12 @@
 import { promises as fs } from 'fs'
-import { connect } from '@saulx/selva'
 import { join as pathJoin } from 'path'
 import { SelvaClient } from '@saulx/selva'
 
 let LAST_BACKUP_TIMESTAMP: number = 0
-let LAST_RUN: number = Date.now()
 
 function msSinceMidnight(d: Date = new Date()) {
   d.setHours(0, 0, 0, 0)
   return Date.now() - d.getTime()
-}
-
-function nextBackupTime(
-  lastBackupTime: number,
-  backupInterval: number
-): number {
-  return Math.ceil(lastBackupTime / backupInterval) * backupInterval
 }
 
 export type BackupFns = { sendBackup: SendBackup; loadBackup: LoadBackup }
@@ -29,12 +20,12 @@ export async function loadBackup(redisDir: string, backupFns: BackupFns) {
   const dumpFile = pathJoin(redisDir, 'dump.rdb')
   try {
     let stat = await fs.stat(dumpFile)
-    console.log(
+    console.info(
       `Existing backup found from ${stat.mtime} of ${stat.size} bytes`
     )
     await backupFns.loadBackup(dumpFile, stat.mtime)
     stat = await fs.stat(dumpFile)
-    console.log(`Backup load completed, size: ${stat.size} bytes`)
+    console.info(`Backup load completed, size: ${stat.size} bytes`)
   } catch (e) {
     await backupFns.loadBackup(dumpFile)
   }
@@ -62,14 +53,14 @@ export function scheduleBackups(
 ) {
   let timeout = null
   const backup = () => {
-    console.log(`Scheduling backup in ${intervalInMinutes} minutes`)
+    console.info(`Scheduling backup in ${intervalInMinutes} minutes`)
     timeout = setTimeout(() => {
       runBackup(redisDir, backupFns)
         .then(() => {
-          console.log('Backup successfully created')
+          console.info('Backup successfully created')
         })
         .catch((e) => {
-          console.log('error', e)
+          console.info('error', e)
         })
         .finally(() => {
           backup()
@@ -90,15 +81,13 @@ export async function runBackup(redisDir: string, backupFns: BackupFns) {
   const dumpPath = pathJoin(redisDir, 'dump.rdb')
   const timeOfDay = msSinceMidnight()
 
-  LAST_RUN = Date.now()
-
   const stat = await fs.stat(dumpPath)
   if (stat.mtime < new Date(LAST_BACKUP_TIMESTAMP)) {
-    console.log(`No changes since ${stat.mtime}, skipping backup`)
+    console.info(`No changes since ${stat.mtime}, skipping backup`)
     return
   }
 
-  console.log('Trying to create backup', String(timeOfDay))
+  console.info('Trying to create backup', String(timeOfDay))
 
   await backupFns.sendBackup(dumpPath)
   LAST_BACKUP_TIMESTAMP = Date.now()
