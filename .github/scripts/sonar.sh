9ba5d2402a2388caa0ced1ce38ff3af7a7ff624e
Olli Vanhoja <olli.vanhoja@gmail.com>
2021-12-22 15:42:19 +0100

    Exclude libdeflate from Sonar for now

--- .github/scripts/sonar.sh
+++ .github/scripts/sonar.sh
@@ -8,7 +8,7 @@ sonar-scanner \
     -Dsonar.organization=atelier-saulx \
     -Dsonar.projectKey=atelier-saulx_selva \
     -Dsonar.sources=server/modules/selva \
-    '-Dsonar.exclusions=server/modules/selva/lib/rmutil/**' \
+    '-Dsonar.exclusions=server/modules/selva/lib/rmutil/**,server/modules/selva/lib/deflate/**' \
     -Dsonar.cfamily.gcov.reportsPath=server/modules/selva/gcov/reports \
     -Dsonar.cfamily.build-wrapper-output=bw-output \
     -Dsonar.cfamily.threads=4 \
