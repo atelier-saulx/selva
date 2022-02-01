ddce8d7835f03d0318de2f186a1a75ce0bd3e0dd
Jim de Beer <beerdejim@gmail.com>
2022-02-01 15:40:40 +0100

    add 5sec delay to syncing

--- client/src/connectRegistry/getInitialRegistryServer.ts
+++ client/src/connectRegistry/getInitialRegistryServer.ts
@@ -7,7 +7,6 @@ import addServer from './addServer'
 const getInitialRegistryServers = async (selvaClient: SelvaClient) => {
   const serverList =
     (await selvaClient.redis.smembers({ type: 'registry' }, 'servers')) || []
-
   await Promise.all(
     serverList.map(async (id: string): Promise<void> => {
       const [host, port, name, type, index] = await selvaClient.redis.hmget(
