8c8178a4e7bcfeed5c9b2d9e2bf3fa67c72f93b2
Jim de Beer <beerdejim@gmail.com>
2022-02-01 16:26:54 +0100

    extra fix for add server ( allways add dont worry )

--- client/src/connectRegistry/syncRegistry.ts
+++ client/src/connectRegistry/syncRegistry.ts
@@ -12,18 +12,16 @@ export const startSync = async (client: SelvaClient) => {
     }
   }
   client.registrySync.isSyncing = true
-  console.info('START SYNC', client.uuid)
   const getServers = async () => {
     await getInitialRegistryServers(client)
     if (client.registrySync?.isSyncing) {
-      client.registrySync.timer = setTimeout(getServers, 5e3)
+      client.registrySync.timer = setTimeout(getServers, 15e3)
     }
   }
-  client.registrySync.timer = setTimeout(getServers, 5e3)
+  client.registrySync.timer = setTimeout(getServers, 15e3)
 }
 
 export const stopSync = async (client: SelvaClient) => {
-  console.info('STOP SYNC', client.uuid)
   clearTimeout(client.registrySync.timer)
   delete client.registrySync
 }
