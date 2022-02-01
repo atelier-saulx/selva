d80bb11e88ecc4c356db72d16e2c5449032207c0
Jim de Beer <beerdejim@gmail.com>
2022-02-01 15:40:40 +0100

    add 5sec delay to syncing

--- client/src/connectRegistry/syncRegistry.ts
+++ client/src/connectRegistry/syncRegistry.ts
@@ -1,21 +1,25 @@
 import { SelvaClient } from '..'
+import getInitialRegistryServers from './getInitialRegistryServer'
 
 export const startSync = async (client: SelvaClient) => {
   // sync with registry once in a while
-
   if (client.registrySync?.isSyncing) {
     return true
   }
-
   if (!client.registrySync) {
     client.registrySync = {
       isSyncing: true,
     }
   }
-
   client.registrySync.isSyncing = true
-
   console.info('START SYNC', client.uuid)
+  const getServers = async () => {
+    await getInitialRegistryServers(client)
+    if (client.registrySync?.isSyncing) {
+      client.registrySync.timer = setTimeout(getServers, 5e3)
+    }
+  }
+  client.registrySync.timer = setTimeout(getServers, 5e3)
 }
 
 export const stopSync = async (client: SelvaClient) => {
