e48f5f6c2575f30e18f501d2297f6884ff904fa7
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-04-20 15:53:07 +0200

    wip sud del bug

--- client/test/subscriptionDelete.ts
+++ client/test/subscriptionDelete.ts
@@ -11,6 +11,28 @@ test.before(async () => {
   srv = await start({
     port,
   })
+
+  await wait(500)
+})
+
+test.beforeEach(async (t) => {
+  const client = connect({ port: port }, { loglevel: 'info' })
+
+  await client.redis.flushall()
+  await client.updateSchema({
+    languages: ['en', 'de', 'nl'],
+    types: {
+      thing: {
+        prefix: 'th',
+        fields: {
+          yesh: { type: 'number' },
+        },
+      },
+    },
+  })
+
+  await wait(100)
+  await client.destroy()
 })
 
 test.after(async (t) => {
@@ -20,17 +42,6 @@ test.after(async (t) => {
 
 test.serial('subscribe and delete', async (t) => {
   const client = connect({ port })
-
-  await client.updateSchema({
-    languages: ['en', 'de', 'nl'],
-    types: {
-      thing: {
-        fields: {
-          yesh: { type: 'number' },
-        },
-      },
-    },
-  })
 
   const q = []
   for (let i = 0; i < 10; i++) {
@@ -86,5 +97,64 @@ test.serial('subscribe and delete', async (t) => {
 
   await wait(1000)
 
+  await client.destroy()
+})
+
+test.serial.only('subscribe and delete descendant', async (t) => {
+  const client = connect({ port })
+
+  console.log('HELLo')
+  const id = await client.set({
+    type: 'thing',
+    yesh: 1,
+    children: [
+      {
+        type: 'thing',
+        $id: 'th2',
+        yesh: 2,
+      }
+    ],
+  })
+
+  console.log('sub to id:', id)
+  const observable = client.observe({
+    $id: id,
+      $language: "en",
+      items: {
+        //parents: true,
+          id: true,
+          $list: {
+            $limit: 1000,
+              $offset: 0,
+              $sort: {
+                $field: "createdAt",
+                  $order: "desc",
+              },
+              $find: {
+                $traverse: "descendants",
+                  $filter: [
+                    {
+                      $field: "type",
+                      $operator: "=",
+                      $value: 'thing',
+                    },
+                  ],
+              },
+          },
+      },
+  })
+
+  observable.subscribe((v) => {
+    console.log('sub got:', v)
+  })
+
+  await wait(100)
+  await client.delete('th2')
+  await wait(100)
+  console.log('Deleted')
+
+  console.log(await client.get({ $id: 'root', descendants: true }))
+
+  await wait(2e3)
   await client.destroy()
 })
