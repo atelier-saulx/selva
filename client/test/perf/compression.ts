441c9d3a5016a0971802b49d0c4976f29e38dfb7
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-05-18 15:28:56 +0200

    Proper perf test for comp

--- client/test/perf/compression.ts
+++ client/test/perf/compression.ts
@@ -1,17 +1,22 @@
-import test from 'ava'
+import { join } from 'path'
 import { performance } from 'perf_hooks'
+import test from 'ava'
 import { connect } from '../../src/index'
 import { start } from '@saulx/selva-server'
-import { wait } from '../assertions'
+import { wait, removeDump } from '../assertions'
 import getPort from 'get-port'
 
+const N = 100;
 let srv
 let port: number
+const dir = join(process.cwd(), 'tmp', 'compression-perf-test')
 
 test.before(async (t) => {
+  removeDump(dir)()
   port = await getPort()
   srv = await start({
     port,
+    dir,
   })
 
   await wait(500)
@@ -62,89 +67,53 @@ test.beforeEach(async (t) => {
     },
   })
 
-  const team1 = await client.id({ type: 'team' })
-  const amount = 50000
-  const vids = 100
-  const genMatches = (s = 0) => {
+  const amount = 100
+  const genVideos = (n) => {
     const ch = []
-    for (let i = s; i < s + amount; i++) {
-      if (i < 1000) {
-        ch.push({
-          type: 'match',
-          flupriflu: 'true',
-          name: 'match' + i,
-          status: i === 0 ? 2 : i > 1000 ? 100 : 300,
-          parents: { $add: team1 },
-        })
-      } else {
-        ch.push({
-          type: 'match',
-          name: 'match' + i,
-          status: 100,
-        })
-      }
-    }
-    return ch
-  }
-
-  const genVideos = () => {
-    const ch = []
-    for (let i = 0; i < vids; i++) {
+    for (let i = 0; i < n; i++) {
       ch.push({
         type: 'video',
         name: 'video',
-        title: { en: 'flap' },
+        title: { en: `Game video ${i}` },
         date: Date.now() + i + (i > 5 ? 1000000 : -100000),
         value: i,
       })
     }
     return ch
   }
+  const genMatches = () => {
+    const ch = []
+    for (let i = 0; i < amount; i++) {
+      for (let j = 0; j < amount; j++) {
+        if (i == j) continue
+        ch.push({
+          type: 'match',
+          flupriflu: 'true',
+          name: `match team${i} vs team${j}`,
+          status: i === 0 ? 2 : i > 1000 ? 100 : 300,
+          parents: [ `team${i}`, `team${j}` ],
+          children: genVideos(4),
+        })
+      }
+    }
 
-  const d = Date.now()
-  const ids = await Promise.all([
-    client.set({
-      type: 'club',
-      name: 'club 1',
-      children: [
-        {
-          $id: team1,
-          name: 'team 1',
-          children: {
-            $add: genVideos(),
-          },
-        },
-      ],
-    }),
+    return ch
+  }
+
+  const d = performance.now()
+  await Promise.all([
     client.set({
       type: 'league',
       name: 'league 1',
       // @ts-ignore
       children: genMatches(),
     }),
-    client.set({
-      type: 'league',
-      name: 'league 2',
-      // @ts-ignore
-      children: genMatches(amount),
-    }),
   ])
-  console.info(
-    `Set ${Math.floor((amount * 2 + vids) / 100) / 10}k nested`,
-    Date.now() - d,
-    'ms'
-  )
+  const tEnd = performance.now() - d
+  const nrIds = (await client.get({ descendants: true })).descendants.length
+  console.info(`Created a hierarchy with ${nrIds} nodes in ${tEnd} ms`)
 
   await wait(600)
-  t.true(ids[0].slice(0, 2) === 'cl' && ids[1].slice(0, 2) === 'le')
-
-  // const matches = (await dumpDb(client)).filter(v => {
-  //   if (typeof v[1] === 'object' && v[1].type === 'match') {
-  //     return true
-  //   }
-  //   return false
-  // })
-
   await client.destroy()
 })
 
@@ -154,13 +123,14 @@ test.after(async (t) => {
   await client.destroy()
   await srv.destroy()
   await t.connectionsAreEmpty()
+  removeDump(dir)()
 })
 
 test.serial.failing('perf: find compression perf - descendants', async (t) => {
   const client = connect({ port }, { loglevel: 'info' })
   await wait(2e3)
 
-  const leagues = (
+  const ids = (
     await client.get({
       n: {
         id: true,
@@ -170,90 +140,87 @@ test.serial.failing('perf: find compression perf - descendants', async (t) => {
             $filter: {
               $field: 'type',
               $operator: '=',
-              $value: 'league',
+              $value: 'match',
             },
           },
         },
       },
     })
   ).n.map(({ id }) => id)
-
-  const start = performance.now()
-  for (let i = 0; i < 1000; i++) {
-    // Compress all leagues
-    // const startCompress = performance.now()
-    await Promise.all(
-      leagues.map(async (id) => {
-        try {
-          // TODO Sometimes we create leagues that cannot be compresed!
-          await client.redis.selva_hierarchy_compress('___selva_hierarchy', id)
-        } catch (e) {}
-      })
-    )
-    // const endCompress = performance.now()
-    // console.log('Compressing leagues took:', endCompress - startCompress);
-
-    await client.get({
-      items: {
-        name: true,
-        value: true,
-        status: true,
-        date: true,
-        id: true,
-        type: true,
-        $list: {
-          $sort: { $field: 'status', $order: 'desc' },
-          $limit: 1000,
-          $find: {
-            $traverse: 'descendants',
-            $filter: [
-              {
+  const q = {
+    items: {
+      name: true,
+      value: true,
+      status: true,
+      date: true,
+      children: true,
+      $list: {
+        $sort: { $field: 'status', $order: 'desc' },
+        $limit: 1000,
+        $find: {
+          $traverse: 'descendants',
+          $filter: [
+            {
+              $operator: '=',
+              $field: 'type',
+              $value: 'match',
+              $and: {
                 $operator: '=',
-                $field: 'type',
-                $value: 'match',
-                $and: {
-                  $operator: '=',
-                  $field: 'status',
-                  $value: [300, 2],
-                },
-                $or: {
-                  $operator: '=',
-                  $field: 'name',
-                  $value: 'league 1',
-                  $or: {
-                    $operator: '>',
-                    $field: 'value',
-                    $value: 4,
-                    $and: {
-                      $operator: '>',
-                      $field: 'value',
-                      $value: 6,
-                      $and: {
-                        $operator: '<',
-                        $field: 'value',
-                        $value: 8,
-                        $and: {
-                          $operator: '>',
-                          $field: 'date',
-                          $value: 'now',
-                        },
-                      },
-                    },
-                  },
-                },
+                $field: 'status',
+                $value: [300, 2],
               },
-              {
-                $operator: '!=',
-                $field: 'name',
-                $value: ['match1', 'match2', 'match3'],
-              },
-            ],
-          },
+            },
+          ],
         },
       },
-    })
+    },
   }
 
-  const time = performance.now() - start
-  console.info(time, 'ms')
+  const compDecimator = 2
+  const compressNodes = async (ids: string[], type: string) => {
+    const startCompress = performance.now()
+
+    await Promise.all(
+      ids.filter((_, i: number) => i % compDecimator == 0).map(async (id: string) => {
+        try {
+          await client.redis.selva_hierarchy_compress('___selva_hierarchy', id)
+        } catch (e) { console.error(e) }
+      })
+    )
+
+    return performance.now() - startCompress
+  }
+
+  // Q1
+  const q1Start = performance.now()
+  for (let i = 0; i < N; i++) {
+    await client.get(q)
+  }
+  const q1TimeTotal = performance.now() - q1Start
+
+  // Q2
+  const q2Start = performance.now()
+  let q2CompTimeTotal = 0;
+  for (let i = 0; i < N; i++) {
+    q2CompTimeTotal += await compressNodes(ids, 'mem')
+    await client.get(q)
+  }
+  const q2TimeTotal = performance.now() - q2Start - q2CompTimeTotal
+
+  // Q3
+  const q3Start = performance.now()
+  let q3CompTimeTotal = 0;
+  for (let i = 0; i < N; i++) {
+    q3CompTimeTotal += await compressNodes(ids, 'disk')
+    await client.get(q)
+  }
+  const q3TimeTotal = performance.now() - q3Start - q3CompTimeTotal
+
+  // Results
+  console.info('N', N)
+  console.info('normal query', q1TimeTotal, 'ms')
+  console.info('compression total (mem)', q2CompTimeTotal, 'ms')
+  console.info('uncompressing query (mem)', q2TimeTotal, 'ms')
+  console.info('compression total (disk)', q3CompTimeTotal, 'ms')
+  console.info('uncompressing query (disk)', q3TimeTotal, 'ms')
 })
