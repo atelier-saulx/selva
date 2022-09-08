823062ba9016469874a30f419ca38391d1baec8c
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-09-08 10:49:26 +0200

    Always return the sub field_name if relevant

--- server/modules/selva/module/subscriptions.c
+++ server/modules/selva/module/subscriptions.c
@@ -2528,7 +2528,7 @@ int SelvaSubscriptions_DebugCommand(RedisModuleCtx *ctx, RedisModuleString **arg
             RedisModule_ReplyWithString(ctx, RedisModule_CreateStringPrintf(ctx, "dir: %s", SelvaTraversal_Dir2str(marker->dir)));
             marker_array_len += 2;
 
-            if (marker->dir & (SELVA_HIERARCHY_TRAVERSAL_REF | SELVA_HIERARCHY_TRAVERSAL_BFS_EDGE_FIELD)) {
+            if (marker->dir & (SELVA_HIERARCHY_TRAVERSAL_REF | SELVA_HIERARCHY_TRAVERSAL_BFS_EDGE_FIELD | SELVA_HIERARCHY_TRAVERSAL_EDGE_FIELD)) {
                 RedisModule_ReplyWithString(ctx, RedisModule_CreateStringPrintf(ctx, "field: %s", marker->ref_field));
                 marker_array_len++;
             }
