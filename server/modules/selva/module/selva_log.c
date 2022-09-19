81c5450cfb5bbe6d7b4e4e581e1c82aabe46775c
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-09-19 12:44:56 +0200

    Implement selva_toggle_dbgmsg()

--- server/modules/selva/module/selva_log.c
+++ server/modules/selva/module/selva_log.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 #include "selva.h"
+
+extern struct _selva_dyndebug_msg __start_dbg_msg;
+extern struct _selva_dyndebug_msg __stop_dbg_msg;
 
 void selva_log(enum selva_log_level level, const char * restrict where, const char * restrict func, const char * restrict fmt, ...) {
     FILE *log_stream = stderr;
@@ -15,4 +19,44 @@ void selva_log(enum selva_log_level level, const char * restrict where, const ch
         fputc('\n', log_stream);
     }
     va_end(args);
+}
+
+int selva_toggle_dbgmsg(char * cfg) {
+    struct _selva_dyndebug_msg * msg_opt = &__start_dbg_msg;
+    struct _selva_dyndebug_msg * stop = &__stop_dbg_msg;
+    char strbuf[80];
+    char * file = strbuf;
+    char * line;
+
+    if (msg_opt == stop) {
+        return SELVA_EINVAL;
+    }
+
+    snprintf(strbuf, sizeof(strbuf), "%s", cfg);
+    file = strbuf;
+    line = strchr(strbuf, ':');
+
+    if (line) {
+        line[0] = '\0';
+        line++;
+    }
+
+    while (msg_opt < stop) {
+        if (strcmp(file, msg_opt->file) == 0) {
+            if (line && *line != '\0') {
+                char msgline[12];
+
+                snprintf(msgline, sizeof(msgline), "%d", msg_opt->line);
+                if (strcmp(line, msgline) != 0) {
+                    goto next;
+                }
+            }
+            msg_opt->flags ^= 1;
+        }
+
+next:
+        msg_opt++;
+    }
+
+    return 0;
 }
