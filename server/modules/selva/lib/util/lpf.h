49f56429a09886b3e963e3910c18c5e37dec23b8
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-09-23 16:23:27 +0200

    Mark LPF functions const

--- server/modules/selva/lib/util/lpf.h
+++ server/modules/selva/lib/util/lpf.h
@@ -9,6 +9,7 @@
 /**
  * Calculate the alpha coefficient for the lpf.
  */
+static inline float lpf_geta(float period, float sample_interval) __attribute__((const));
 static inline float lpf_geta(float period, float sample_interval) {
     return __builtin_expf(-(sample_interval / period));
 }
@@ -19,6 +20,7 @@ static inline float lpf_geta(float period, float sample_interval) {
  * @param a is the coefficient calculated by lpf_geta().
  * @param sample is the current sample.
  */
+static inline float lpf_calc_next(float a, float prev, float sample) __attribute__((const));
 static inline float lpf_calc_next(float a, float prev, float sample) {
     return a * prev + (1.0f - a) * sample;
 }
