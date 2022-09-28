eb20e4fd6de78fa8b18066978d17c6340f781dad
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-09-28 11:05:07 +0200

    Add a unit test for crc32c

---
+++ server/modules/selva/test/units/test-crc32c.c
@@ -0,0 +1,27 @@
+#include <punit.h>
+#include <stdint.h>
+#include "crc32c.h"
+
+static void setup(void)
+{
+}
+
+static void teardown(void)
+{
+}
+
+static char * test_check(void)
+{
+    const char check1[] = "123456789";
+    const char check2[] = "Hello world!";
+
+    pu_assert_equal("should match", crc32c(0, check1, sizeof(check1) - 1), 0xe3069283);
+    pu_assert_equal("should match", crc32c(0, check2, sizeof(check2) - 1), 0x7b98e751);
+
+    return NULL;
+}
+
+void all_tests(void)
+{
+    pu_def_test(test_check, PU_RUN);
+}
---
+++ server/modules/selva/test/units/test-crc32c.mk
@@ -0,0 +1,2 @@
+TEST_SRC += test-crc32c.c
+SRC-crc32c += ../../lib/util/crc32c/crc32c.c
