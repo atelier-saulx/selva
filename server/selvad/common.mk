d3efe9699957a263c55a222fa01ff9168dbd070c
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-10-27 10:08:47 +0200

    Slightly more deterministic builds

--- server/selvad/common.mk
+++ server/selvad/common.mk
@@ -7,8 +7,14 @@
 # OS name (Linux, Darwin)
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 
+# Set _DATE__ and __TIME__ macros to a deterministic value
+export SOURCE_DATE_EPOCH := $(git log -1 --pretty=%ct)
+export ZERO_AR_DATE := 1
+
 # CFLAGS shared with all compilation units.
 CFLAGS := -MMD -Wall -Wextra
+# See substitute-path in .gdbinit
+CFLAGS += -ffile-prefix-map=$(PWD)=/src
 
 ifeq ($(uname_S),Linux) # Assume Intel x86-64 Linux
 	CFLAGS += -g -ggdb3 -march=x86-64 -mtune=intel -mfpmath=sse -mavx -mavx2 -mbmi -mbmi2 -mlzcnt -mmovbe -mprfchw -fno-math-errno -ftree-vectorize
