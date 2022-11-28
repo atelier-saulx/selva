3fd008678a95c92150d786d8f5175ab71da9afb7
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-11-28 15:16:30 +0100

    Fix hierarchy types cmds

--- server/selvad/modules/db/module/hierarchy/types.c
+++ server/selvad/modules/db/module/hierarchy/types.c
@@ -4,12 +4,14 @@
  */
 #include <stddef.h>
 #include <sys/types.h>
+#include "util/finalizer.h"
 #include "util/selva_string.h"
 #include "selva_error.h"
 #include "selva_server.h"
 #include "selva_db.h"
 #include "selva_onload.h"
 #include "selva_object.h"
+#include "arg_parser.h"
 #include "hierarchy.h"
 
 /**
@@ -37,17 +39,27 @@ struct selva_string *SelvaHierarchyTypes_Get(struct SelvaHierarchy *hierarchy, c
     return out;
 }
 
-int SelvaHierarchyTypes_AddCommand(struct selva_server_response_out *resp, struct selva_string **argv, int argc) {
+void SelvaHierarchyTypes_AddCommand(struct selva_server_response_out *resp, const void *buf, size_t len) {
     struct SelvaHierarchy *hierarchy = main_hierarchy;
+    __auto_finalizer struct finalizer fin;
+    struct selva_string **argv;
+    int argc;
     struct selva_string *type;
     struct selva_string *name;
     int err;
 
+    finalizer_init(&fin);
+
     const int ARGV_TYPE = 2;
     const int ARGV_NAME = 3;
 
-    if (argc != 4) {
-        return selva_send_error_arity(resp);
+    argc = SelvaArgParser_buf2strings(&fin, buf, len, &argv);
+    if (argc < 0) {
+        selva_send_errorf(resp, argc, "Failed to parse args");
+        return;
+    } else if (argc != 4) {
+        selva_send_error_arity(resp);
+        return;
     }
 
     type = argv[ARGV_TYPE];
@@ -55,28 +67,34 @@ int SelvaHierarchyTypes_AddCommand(struct selva_server_response_out *resp, struc
     TO_STR(type, name);
 
     if (type_len != 2) {
-        return selva_send_error(resp, SELVA_EINTYPE, NULL, 0);
+        selva_send_error(resp, SELVA_EINTYPE, NULL, 0);
+        return;
     }
 
     err = SelvaHierarchyTypes_Add(hierarchy, type_str, name_str, name_len);
     if (err) {
-        return selva_send_error(resp, err, NULL, 0);
+        selva_send_error(resp, err, NULL, 0);
+        return;
     }
 
-    return selva_send_ll(resp, 1);
+    selva_send_ll(resp, 1);
 }
 
-int SelvaHierarchyTypes_ClearCommand(struct selva_server_response_out *resp, struct selva_string **argv, int argc) {
-    struct SelvaHierarchy *hierarchy = main_hierarchy;
-
-    SelvaHierarchyTypes_Clear(hierarchy);
-    return selva_send_ll(resp, 1);
+void SelvaHierarchyTypes_ClearCommand(struct selva_server_response_out *resp, const void *buf __unused, size_t len) {
+    if (len != 0) {
+        selva_send_error_arity(resp);
+    } else {
+        SelvaHierarchyTypes_Clear(main_hierarchy);
+        selva_send_ll(resp, 1);
+    }
 }
 
-int SelvaHierarchyTypes_ListCommand(struct selva_server_response_out *resp, struct selva_string **argv, int argc) {
-    struct SelvaHierarchy *hierarchy = main_hierarchy;
-
-    return SelvaObject_ReplyWithObject(resp, NULL, SELVA_HIERARCHY_GET_TYPES_OBJ(hierarchy), NULL, 0);
+void SelvaHierarchyTypes_ListCommand(struct selva_server_response_out *resp, const void *buf __unused, size_t len) {
+    if (len != 0) {
+        selva_send_error_arity(resp);
+    } else {
+        SelvaObject_ReplyWithObject(resp, NULL, SELVA_HIERARCHY_GET_TYPES_OBJ(main_hierarchy), NULL, 0);
+    }
 }
 
 static int SelvaHierarchyTypes_OnLoad(void) {
