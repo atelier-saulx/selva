e0e865e5f1c8d3ef3e461e009c73ae9cfe40d94d
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-02-02 09:38:36 +0100

    Add some comments to trigger Sonar

--- server/selvad/modules/replication/replica/replicaof.h
+++ server/selvad/modules/replication/replica/replicaof.h
@@ -4,7 +4,15 @@
  */
 #pragma once
 
+/**
+ * Connect to origin server for replication.
+ * @returns a socket to the origin.
+ */
 [[nodiscard]]
 int replication_replica_connect_to_origin(struct sockaddr_in *origin_addr);
 
+/**
+ * Start replicating from an origin server.
+ * @param sock is a socket returned by replication_replica_connect_to_origin().
+ */
 int replication_replica_start(int sock);
