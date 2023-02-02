19a5c49478f68664ae3982dbd9251f6fcd0dba11
Tony Kovanen <rase-@users.noreply.github.com>
2023-02-02 10:20:47 +0100

    Buffer 'now' value refresh checks (#453)
    * shared refresh check logic
    
    * buffer refresh checks

--- client/src/get/executeGetOperations/aggregate.ts
+++ client/src/get/executeGetOperations/aggregate.ts
@@ -1,6 +1,6 @@
 import { Schema, SelvaClient } from '../../'
 import { GetOperationAggregate, GetOperationFind } from '../types'
-import { findIds } from './find'
+import { bufferRefreshCheck, findIds } from './find'
 import {
   ast2rpn,
   Fork,
@@ -28,181 +28,6 @@ const FN_TO_ENUM = {
   avg: '3',
   min: '4',
   max: '5',
-}
-
-function findTimebased(ast: Fork): FilterAST[] {
-  if (!ast) {
-    return []
-  }
-
-  const parse = (fork: Fork, filters: FilterAST[]) => {
-    if (fork.$and) {
-      for (const f of fork.$and) {
-        if (isFork(f)) {
-          parse(f, filters)
-        } else if (f.hasNow) {
-          filters.push(f)
-        }
-      }
-    } else if (fork.$or) {
-      for (const f of fork.$or) {
-        if (isFork(f)) {
-          parse(f, filters)
-        } else if (f.hasNow) {
-          filters.push(f)
-        }
-      }
-    }
-  }
-
-  const res = []
-  parse(ast, res)
-  return res
-}
-
-function excludeTimebased(ast: Fork | FilterAST): Fork | FilterAST {
-  if (!isFork(ast)) {
-    return ast
-  }
-
-  const newFork = Object.assign({}, ast)
-  const filters = []
-  if (ast.$or) {
-    for (const f of ast.$or) {
-      if (isFork(f)) {
-        const n = excludeTimebased(f)
-        if (n) {
-          filters.push(n)
-        }
-      } else if (!f.hasNow) {
-        filters.push(f)
-      }
-    }
-
-    newFork.$or = filters
-  } else if (ast.$and) {
-    for (const f of ast.$and) {
-      if (isFork(f)) {
-        const n = excludeTimebased(f)
-        if (n) {
-          filters.push(n)
-        }
-      } else if (!f.hasNow) {
-        filters.push(f)
-      }
-    }
-
-    newFork.$and = filters
-  }
-
-  if (!filters.length) {
-    return null
-  }
-
-  return newFork
-}
-
-async function checkForNextRefresh(
-  ctx: ExecContext,
-  client: SelvaClient,
-  sourceField: string,
-  paddedIds: string,
-  ast: Fork,
-  lang?: string
-): Promise<void> {
-  if (!ctx.subId) {
-    return
-  }
-
-  const uniq = new Set()
-  const timebased = findTimebased(ast).filter((f) => {
-    if (uniq.has(f.$field)) {
-      return false
-    }
-
-    uniq.add(f.$field)
-    return true
-  })
-
-  if (!timebased.length) {
-    return
-  }
-
-  const withoutTimebased = excludeTimebased(ast)
-  await Promise.all(
-    timebased.map(async (f) => {
-      // console.log('TRYING TIMEBASED')
-      const newFilter: FilterAST = {
-        $operator: '>',
-        $value: f.$value,
-        $field: f.$field,
-      }
-
-      let newFork: Fork = {
-        isFork: true,
-        $and: [withoutTimebased, newFilter],
-      }
-
-      if (!withoutTimebased) {
-        newFork.$and = [newFilter]
-      }
-
-      const args = ast2rpn(client.schemas[ctx.db].types, newFork, lang)
-      // TODO
-      const ids = await client.redis.selva_hierarchy_find(
-        ctx.originDescriptors[ctx.db] || { name: ctx.db },
-        makeLangArg(client.schemas[ctx.db].languages, lang),
-        '___selva_hierarchy',
-        // TODO proper dir, needs byType
-        ...sourceFieldToFindArgs(
-          client.schemas[ctx.db],
-          null,
-          sourceField,
-          false
-        ),
-        'order',
-        f.$field,
-        'asc',
-        'offset',
-        0,
-        'limit',
-        1,
-        paddedIds,
-        ...args
-      )
-
-      if (!ids || !ids.length) {
-        return
-      }
-
-      const [id] = ids
-
-      const time = Number(
-        await client.redis.selva_object_get(
-          ctx.originDescriptors[ctx.db] || { name: ctx.db },
-          makeLangArg(client.schemas[ctx.db].languages, lang),
-          id,
-          f.$field
-        )
-      )
-
-      let v = <string>f.$value
-      if (v.startsWith('now-')) {
-        v = v.replace('now-', 'now+')
-      } else if (v.startsWith('now+')) {
-        v = v.replace('now+', 'now-')
-      }
-
-      let converted = convertNow(v, time)
-      // console.log('TIME NOW', Date.now())
-      // console.log('NEXT TIME', time)
-      // console.log('ADJUSTED', converted)
-
-      if (!ctx.meta.___refreshAt || ctx.meta.___refreshAt > converted) {
-        ctx.meta.___refreshAt = converted
-      }
-    })
-  )
 }
 
 // TODO: implement recursive version
@@ -317,7 +142,7 @@ const executeAggregateOperation = async (
       ...args
     )
 
-    await checkForNextRefresh(
+    bufferRefreshCheck(
       ctx,
       client,
       sourceField,
@@ -358,7 +183,7 @@ const executeAggregateOperation = async (
           rpn: args,
         })
 
-        await checkForNextRefresh(ctx, client, sourceField, id, op.filter, lang)
+        bufferRefreshCheck(ctx, client, sourceField, id, op.filter, lang)
       }
     } else {
       const schema = client.schemas[ctx.db]
@@ -407,14 +232,7 @@ const executeAggregateOperation = async (
       ...args
     )
 
-    await checkForNextRefresh(
-      ctx,
-      client,
-      sourceField,
-      padId(op.id),
-      op.filter,
-      lang
-    )
+    bufferRefreshCheck(ctx, client, sourceField, padId(op.id), op.filter, lang)
 
     return Number(agg)
   }
