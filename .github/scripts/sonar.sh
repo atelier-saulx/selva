5fb5f74573820eef24797069d84bc255daa113c9
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-02-02 10:31:10 +0100

    Sonar: Igonre ng for now as Sonar doesn't support C23

--- .github/scripts/sonar.sh
+++ .github/scripts/sonar.sh
@@ -3,7 +3,7 @@ set -e
 make -C server/selvad clean
 # TODO make test fails in GH Actions
 #build-wrapper-linux-x86-64 --out-dir bw-output make all test test-gcov -C server/modules/selva -j4
-build-wrapper-linux-x86-64 --out-dir bw-output make all -C server/selvad
+build-wrapper-linux-x86-64 --out-dir bw-output make all -C server/selvad -j2
 sonar-scanner \
     -Dsonar.organization=atelier-saulx \
     -Dsonar.projectKey=atelier-saulx_selva \
@@ -11,5 +11,5 @@ sonar-scanner \
     '-Dsonar.exclusions=server/selvad/lib/deflate/**,server/selvad/lib/jemalloc/**,server/selvad/include/jemalloc_*.h' \
     -Dsonar.cfamily.gcov.reportsPath=server/selvad/gcov/reports \
     -Dsonar.cfamily.build-wrapper-output=bw-output \
-    -Dsonar.cfamily.threads=4 \
+    -Dsonar.cfamily.threads=2 \
     -Dsonar.host.url=https://sonarcloud.io
