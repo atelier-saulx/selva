29a071dd3f311bde8c69784dad7a27e2b9d91339
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-02-21 14:27:02 +0100

    Revert "Revert "Revert some changes to replica handling""
    This reverts commit 42244425a135826920b746c22929f0b7cfaf4012.

--- server/selvad/modules/replication/replica/replication.h
+++ server/selvad/modules/replication/replica/replication.h
@@ -4,22 +4,15 @@
  */
 #pragma once
 
-struct replication_sock_state;
-
-struct replication_sock_state *replication_replica_init(void);
-
 /**
  * Connect to origin server for replication.
  * @returns a socket to the origin.
  */
 [[nodiscard]]
-int replication_replica_connect_to_origin(struct replication_sock_state *sv, struct sockaddr_in *origin_addr);
+int replication_replica_connect_to_origin(struct sockaddr_in *origin_addr);
 
 /**
  * Start replicating from an origin server.
- * First call replication_replica_init(),
- * then replication_replica_connect_to_origin(),
- * and finally this function.
- * Freeing sv in case of an error is handled by these functions.
+ * @param sock is a socket returned by replication_replica_connect_to_origin().
  */
-int replication_replica_start(struct replication_sock_state *sv);
+int replication_replica_start(int sock);
