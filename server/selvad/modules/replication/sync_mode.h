cfbdc3b94762cc3c5cd966c7fee9172989c00db9
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-03-02 16:27:21 +0100

    Add sync mode header

---
+++ server/selvad/modules/replication/sync_mode.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023 SAULX
+ * SPDX-License-Identifier: MIT
+ */
+#pragma once
+
+enum replication_sync_mode {
+    REPLICATION_SYNC_MODE_FULL = 0,
+    REPLICATION_SYNC_MODE_PARTIAL,
+};
