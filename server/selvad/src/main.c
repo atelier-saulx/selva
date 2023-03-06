8067545c648378d6486ed66c944c2a419627f7e5
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-03-06 14:26:07 +0100

    Exit if module load fails

--- server/selvad/src/main.c
+++ server/selvad/src/main.c
@@ -1,28 +1,36 @@
 /*
- * Copyright (c) 2022 SAULX
+ * Copyright (c) 2022-2023 SAULX
  * SPDX-License-Identifier: MIT
  */
+#include <stdlib.h>
 #include "selva_log.h"
 #include "event_loop.h"
 #include "module.h"
+
+static const char *modules[] = {
+    "mod_signal.so",
+#if 0
+    "mod_demo_timeout.so",
+    "mod_demo_async.so",
+    "mod_demo_sock.so",
+#endif
+    "mod_server.so",
+    "mod_replication.so",
+    "mod_io.so",
+    "mod_db.so",
+};
 
 int main(void)
 {
     evl_init();
 
-    /* TODO Fail if mod load fails */
-    SELVA_LOG(SELVA_LOGL_INFO, "Selva %s", __DATE__);
+    SELVA_LOG(SELVA_LOGL_INFO, "Selva build: %s", __DATE__);
 
-    evl_load_module("mod_signal.so");
-#if 0
-    evl_load_module("mod_demo_timeout.so");
-    evl_load_module("mod_demo_async.so");
-    evl_load_module("mod_demo_sock.so");
-#endif
-    evl_load_module("mod_server.so");
-    evl_load_module("mod_replication.so");
-    evl_load_module("mod_io.so");
-    evl_load_module("mod_db.so");
+    for (size_t i = 0; i < num_elem(modules); i++) {
+        if (!evl_load_module(modules[i])) {
+            exit(EXIT_FAILURE);
+        }
+    }
 
     evl_start();
     evl_deinit();
