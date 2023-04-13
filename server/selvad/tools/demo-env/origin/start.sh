dc08ece7d4048471337753ace4022f9f588e2dfa
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-04-13 16:28:35 +0200

    Add missing copyright

--- server/selvad/tools/demo-env/origin/start.sh
+++ server/selvad/tools/demo-env/origin/start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+# Copyright (c) 2023 SAULX
+#
+# SPDX-License-Identifier: MIT
 #SELVA_MALLOC_CONF="prof:true,prof_active:true,prof_leak:true,lg_prof_interval:30,lg_prof_sample:17,prof_prefix:jeprof.out"
 LOCPATH=../../../binaries/Linux_x86_64/locale SELVA_PORT=3000 SELVA_REPLICATION_MODE=1 AUTO_SAVE_INTERVAL=1200 exec ../../../selvad
--- server/selvad/tools/demo-env/replica-1/start.sh
+++ server/selvad/tools/demo-env/replica-1/start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# Copyright (c) 2023 SAULX
+#
+# SPDX-License-Identifier: MIT
 LOCPATH=../../../binaries/Linux_x86_64/locale SELVA_PORT=3001 SELVA_REPLICATION_MODE=2 exec ../../../selvad
