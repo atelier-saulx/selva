0d3e5321ba086ff3aae27bc9dcb9b5ac685fc29c
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-04-14 17:24:46 +0200

    Use SO_RCVLOWAT

--- server/selvad/modules/server/server.c
+++ server/selvad/modules/server/server.c
@@ -451,6 +451,9 @@ static void on_connection(struct event *event, void *arg __unused)
     tcp_set_nodelay(new_sockfd);
     tcp_set_keepalive(new_sockfd, TCP_KEEPALIVE_TIME, TCP_KEEPALIVE_INTVL, TCP_KEEPALIVE_PROBES);
 
+    /* selva_proto will never see a chunk smaller than this. */
+    (void)setsockopt(new_sockfd, SOL_SOCKET, SO_RCVLOWAT, &(int){8}, sizeof(int));
+
     inet_ntop(AF_INET, &client.sin_addr, buf, sizeof(buf));
     SELVA_LOG(SELVA_LOGL_INFO, "Received a connection from %s:%d", buf, ntohs(client.sin_port));
 
