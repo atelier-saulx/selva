84e329bda05c71be12b50acc9c33f97f0e3ee056
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-05-25 16:01:27 +0200

    Add some doc comments

--- server/selvad/include/util/hll.h
+++ server/selvad/include/util/hll.h
@@ -6,10 +6,37 @@
 
 typedef void hll_t;
 
+/**
+ * Create a new HyperLogLog data structure.
+ */
 hll_t *hll_create(void);
+
+/**
+ * Destroy a HyperLogLog data structure.
+ */
 void hll_destroy(hll_t *ptr);
+
+/**
+ * Size of a HyperLogLog data structure in bytes.
+ */
 size_t hll_bsize(hll_t *ptr);
+
+/**
+ * Add an element to the HyperLogLog data structure.
+ */
 int hll_add(hll_t **ptr, const unsigned char *ele, size_t elesize);
+
+/**
+ * Galculate the Hyperloglog cardinality.
+ */
 long long hll_count(hll_t *ptr);
+
+/**
+ * Merge the Hyperloglog structure src into dst.
+ */
 int hll_merge(hll_t **dest, hll_t *src);
+
+/**
+ * Convert a Hyperloglog data struct stored in a sparse representation into a dense representation.
+ */
 hll_t *hll_sparse_to_dense(hll_t *ptr);
