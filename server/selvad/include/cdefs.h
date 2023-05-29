029f0e4ee7fddab367b4d0ba460fc9e06a26682a
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-05-29 14:48:46 +0200

    Remove RFE comment

--- server/selvad/include/cdefs.h
+++ server/selvad/include/cdefs.h
@@ -85,8 +85,6 @@
 
 /*
  * TODO typeof_unqual() will probably make containerofa unnecessary.
- * RFE the type checking could be stronger similar to Linux
- * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=c7acec713d14c6ce8a20154f9dfda258d6bcad3b
  */
 #define containerofa(a, s, m) \
     ((s *)((uint8_t *)((void *)(a)) - offsetof(s, m)))
